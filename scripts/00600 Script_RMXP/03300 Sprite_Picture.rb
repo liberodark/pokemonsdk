3ecea0bef71224684abf59d24f19089978c86b6d
Nuri Yuri <youri.54@live.fr>
2018-12-26 17:41:09 +0100

    Sprite_Picture: Add blend_type & tone processing.

--- scripts/00600 Script_RMXP/03300 Sprite_Picture.rb
+++ scripts/00600 Script_RMXP/03300 Sprite_Picture.rb
@@ -1,12 +1,28 @@
 #encoding: utf-8
 
 # A sprite that show a Game_Picture on the screen
-class Sprite_Picture < Sprite
+class Sprite_Picture < ShaderedSprite
+  SPRITE_SHADER = <<-EOSHADER
+  uniform vec4 tone;
+  const vec3 lumaF = vec3(.299, .587, .114);
+  uniform sampler2D texture;
+  void main()
+  {
+    vec4 frag = texture2D(texture, gl_TexCoord[0].xy);
+    float luma = dot(frag.rgb, lumaF);
+    frag.rgb += tone.rgb;
+    frag.rgb = mix(frag.rgb, vec3(luma), tone.w);
+    frag.a *= gl_Color.a;
+    // Result
+    gl_FragColor = frag;
+  }
+  EOSHADER
   # Initialize a new Sprite_Picture
   # @param viewport [Viewport] the viewport where the sprite will be shown
   # @param picture [Game_Picture] the picture
   def initialize(viewport, picture)
     super(viewport)
+    self.shader = Shader.new(SPRITE_SHADER)
     @picture = picture
     update
   end
@@ -54,9 +70,14 @@ class Sprite_Picture < Sprite
     self.zoom_x = @picture.zoom_x / 100.0
     self.zoom_y = @picture.zoom_y / 100.0
     self.opacity = @picture.opacity
-#    self.blend_type = @picture.blend_type
+    shader.blend_type = @picture.blend_type
     # 回転角度、色調を設定
     self.angle = @picture.angle
 #    self.tone = @picture.tone
+    tone = @picture.tone
+    unless tone.eql?(@current_tone)
+      shader.set_float_uniform('tone', tone)
+      @current_tone = tone.clone
+    end
   end
 end
