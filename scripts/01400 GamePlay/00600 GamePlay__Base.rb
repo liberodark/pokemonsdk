caaff48b5d3b14a57ada9888d225c67ffb8076d4
Nuri Yuri <youri.54@live.fr>
2019-04-27 14:04:05 +0200

    GamePlay::Base add fade options & changed visibility of methods

--- scripts/01400 GamePlay/00600 GamePlay__Base.rb
+++ scripts/01400 GamePlay/00600 GamePlay__Base.rb
@@ -2,12 +2,18 @@
 
 module GamePlay
   # The base class of every GamePlay scene interface
-  # 
+  #
   # Add some usefull functions like message display and scene switch
   # @author Nuri Yuri
   class Base
+    # Default fade type used to switch between interfaces
+    # @return [Symbol] :transition (for Graphics.freeze/transition), :fade_bk (for fade through black)
+    DEFAULT_TRANSITION = :transition
+    # Parameters of the transition
+    # @return [Integer, Array] (usually the number of frame for the transition)
+    DEFAULT_TRANSITION_PARAMETER = 16
     # Message the displays when a GamePlay scene has been initialized without message processing and try to display a message
-    MessageError = 'This interface has no MessageWindow, you cannot call display_message'
+    MESSAGE_ERROR = 'This interface has no MessageWindow, you cannot call display_message'
     ::PFM::Text.define_const(self)
     include Sprites
     include Input
@@ -37,28 +43,6 @@ module GamePlay
       # Store the current scene
       @__last_scene = $scene
       _init_sprites
-    end
-
-    # Initialize the window related interface of the UI
-    # @param no_message [Boolean] if the scene is created wihout the message management
-    # @param message_z [Integer] the z superiority of the message
-    # @param message_viewport_args [Array] if empty : [:main, message_z] will be used.
-    def message_initialize(no_message, message_z, message_viewport_args)
-      if no_message.is_a?(::Yuki::Message)
-        @message_window = no_message
-        @inherited_message_window = true
-      elsif no_message
-        @message_window = false
-      else
-        # if $game_temp.in_battle
-        #  @message_window = ::Scene_Battle::Window_Message.new
-        #  @message_window.wait_input = true
-        # else
-        message_viewport_args = [:main, message_z] if message_viewport_args.empty?
-        @message_window = message_class.new(Viewport.create(*message_viewport_args), self)
-        # end
-        @message_window.z = message_z
-      end
     end
 
     # Scene update process
@@ -95,25 +79,6 @@ module GamePlay
       $scene = @__last_scene if $scene == self
     end
 
-    # The main process at the begin of scene
-    def main_begin
-      Graphics.transition
-    end
-
-    # The main process (block until scene stop running)
-    def main_process
-      while @running
-        Graphics.update
-        update
-      end
-    end
-
-    # The main process at the end of the scene (when scene is not running anymore)
-    def main_end
-      Graphics.freeze
-      dispose
-    end
-
     # Change the viewport visibility of the scene
     # @param value [Boolean]
     def visible=(value)
@@ -121,64 +86,20 @@ module GamePlay
       @message_window.viewport.visible = value if @message_window
     end
 
-    # Call an other scene
-    # @param name [Class] the scene to call
-    # @param args [Array] the parameter of the initialize method of the scene to call
-    # @return [Boolean] if this scene can still run
-    def call_scene(name, *args)
-      Graphics.freeze
-      # Make the current scene invisible
-      self.visible = false
-      result_process = @__result_process
-      @__result_process = nil
-      scene = name.new(*args)
-      scene.main
-      # Call the result process if any
-      result_process.call(scene) if result_process
-      # If the scene has changed we stop this one
-      return @running = false if $scene != self or !@running
-      self.visible = true
-      Graphics.transition
-      return true
-    end
-
-    # Return to an other scene, create the scene if args.size > 0
-    # @param name [Class] the scene to return to
-    # @param args [Array] the parameter of the initialize method of the scene to call
-    # @note This scene will stop running
-    # @return [Boolean] if the scene has successfully returned to the desired scene
-    def return_to_scene(name, *args)
-      if args.empty?
-        scene = self
-        while scene.is_a?(Base)
-          scene = scene.__last_scene
-          break if scene == self
-          next unless scene.class == name
-          $scene = scene
-          @running = false
-          return true
-        end
-        return false
-      end
-      $scene = name.new(*args)
-      @running = false
-      return true
-    end
-
     # Display a message with choice or not
     # @param message [String] the message to display
     # @param start [Integer] the start choice index (1..nb_choice)
     # @param choices [Array<String>] the list of choice options
     # @return [Integer, nil] the choice result
-    def display_message(message, start=1, *choices)
-      raise ScriptError, MessageError unless @message_window
+    def display_message(message, start = 1, *choices)
+      raise ScriptError, MESSAGE_ERROR unless @message_window
       # message = @message_window.contents.multiline_calibrate(message)
       $game_temp.message_text = message
       processing_message = true
       $game_temp.message_proc = proc { processing_message = false }
       # Choice management
       choice = nil
-      if !choices.empty?
+      unless choices.empty?
         $game_temp.choice_max = choices.size
         $game_temp.choice_cancel_type = choices.size
         $game_temp.choice_proc = proc { |i| choice = i }
@@ -191,7 +112,7 @@ module GamePlay
         Graphics.update
         @message_window.update
         @__display_message_proc&.call
-        if edit_max and @message_window.input_number_window
+        if edit_max && @message_window.input_number_window
           edit_max = false
           @message_window.input_number_window.max = $game_temp.num_input_start
         end
@@ -205,13 +126,56 @@ module GamePlay
     # @param start [Integer] the start choice index (1..nb_choice)
     # @param choices [Array<String>] the list of choice options
     # @return [Integer, nil] the choice result
-    def display_message_and_wait(message, start=1, *choices)
+    def display_message_and_wait(message, start = 1, *choices)
       choice = display_message(message, start, *choices)
       while $game_temp.message_window_showing
         Graphics.update
         @message_window.update
       end
       return choice
+    end
+
+    private
+
+    # The main process at the begin of scene
+    def main_begin
+      fade_in(@mbf_type || DEFAULT_TRANSITION, @mbf_param || DEFAULT_TRANSITION_PARAMETER)
+    end
+
+    # The main process (block until scene stop running)
+    def main_process
+      while @running
+        Graphics.update
+        update
+      end
+    end
+
+    # The main process at the end of the scene (when scene is not running anymore)
+    def main_end
+      fade_out(@mef_type || DEFAULT_TRANSITION, @mef_param || DEFAULT_TRANSITION_PARAMETER)
+      dispose
+    end
+
+    # Initialize the window related interface of the UI
+    # @param no_message [Boolean] if the scene is created wihout the message management
+    # @param message_z [Integer] the z superiority of the message
+    # @param message_viewport_args [Array] if empty : [:main, message_z] will be used.
+    def message_initialize(no_message, message_z, message_viewport_args)
+      if no_message.is_a?(::Yuki::Message)
+        @message_window = no_message
+        @inherited_message_window = true
+      elsif no_message
+        @message_window = false
+      else
+        # if $game_temp.in_battle
+        #  @message_window = ::Scene_Battle::Window_Message.new
+        #  @message_window.wait_input = true
+        # else
+        message_viewport_args = [:main, message_z] if message_viewport_args.empty?
+        @message_window = message_class.new(Viewport.create(*message_viewport_args), self)
+        # end
+        @message_window.z = message_z
+      end
     end
 
     # Perform an index change test and update the index (rotative)
@@ -273,12 +237,118 @@ module GamePlay
       end
     end
 
-    private
+    # Call an other scene
+    # @param name [Class] the scene to call
+    # @param args [Array] the parameter of the initialize method of the scene to call
+    # @return [Boolean] if this scene can still run
+    def call_scene(name, *args, &result_process)
+      fade_out(@cfo_type || DEFAULT_TRANSITION, @cfo_param || DEFAULT_TRANSITION_PARAMETER)
+      # Make the current scene invisible
+      self.visible = false
+      result_process ||= @__result_process
+      @__result_process = nil
+      scene = name.new(*args)
+      scene.main
+      # Call the result process if any
+      result_process&.call(scene)
+      # If the scene has changed we stop this one
+      return @running = false if $scene != self || !@running
+      self.visible = true
+      fade_in(@cfi_type || DEFAULT_TRANSITION, @cfi_param || DEFAULT_TRANSITION_PARAMETER)
+      return true
+    end
+
+    # Return to an other scene, create the scene if args.size > 0
+    # @param name [Class] the scene to return to
+    # @param args [Array] the parameter of the initialize method of the scene to call
+    # @note This scene will stop running
+    # @return [Boolean] if the scene has successfully returned to the desired scene
+    def return_to_scene(name, *args)
+      if args.empty?
+        scene = self
+        while scene.is_a?(Base)
+          scene = scene.__last_scene
+          break if scene == self
+          next unless scene.class == name
+          $scene = scene
+          @running = false
+          return true
+        end
+        return false
+      end
+      $scene = name.new(*args)
+      @running = false
+      return true
+    end
 
     # Return the message class used
     # @return [Class]
     def message_class
       Yuki::Message
+    end
+
+    # Process the fade out process (going through black)
+    # @param type [Symbol] type of transition
+    # @param parameters [Integer, Array] parameters of the transition
+    def fade_out(type, parameters)
+      case type
+      when :transition
+        Graphics.freeze
+        Graphics.brightness = 255
+      when :fade_bk
+        (parameters - 1).downto(0) do |i|
+          Graphics.brightness = i * 255 / parameters
+          Graphics.update
+        end
+      end
+    end
+
+    # Process the fade in process
+    # @param type [Symbol] type of transition
+    # @param parameters [Integer, Array] parameters of the transition
+    def fade_in(type, parameters)
+      case type
+      when :transition
+        Graphics.brightness = 255
+        Graphics.transition(*parameters)
+      when :fade_bk
+        1.upto(parameters) do |i|
+          Graphics.brightness = i * 255 / parameters
+          Graphics.update
+        end
+      end
+    end
+
+    # Define the transition info when the scene start
+    # @param type [Symbol] type of transition
+    # @param parameters [Integer, Array] parameters of the transition
+    def define_main_begin_fade(type, parameters = nil)
+      @mbf_type = type
+      @mbf_param = parameters
+    end
+
+    # Define the transition info when the scene stops
+    # @param type [Symbol] type of transition
+    # @param parameters [Integer, Array] parameters of the transition
+    def define_main_end_fade(type, parameters = nil)
+      @mef_type = type
+      @mef_param = parameters
+    end
+
+    # Define the transition info when we switch to the called scene
+    # @param type [Symbol] type of transition
+    # @param parameters [Integer, Array] parameters of the transition
+    def define_call_scene_fade_out(type, parameters = nil)
+      @cfo_type = type
+      @cfo_param = parameters
+    end
+
+    # Define the transition info when we return from call_scene
+    # @param type [Symbol] type of transition
+    # @param parameters [Integer, Array] parameters of the transition
+    def define_call_scene_fade_in(type, parameters = nil)
+      @cfi_type = type
+      @cfi_param = parameters
     end
   end
 end
