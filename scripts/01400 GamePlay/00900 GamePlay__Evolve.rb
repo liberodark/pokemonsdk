601e2e4a699452d3630438a1367e21eeb8b89670
Leikt Sol'Reihin <leikt.solreihin@gmail.com>
2019-06-04 17:11:45 +0000

    Disabled dex mark seens only the caught pokemons + Pkm change in descr window

--- scripts/01400 GamePlay/00900 GamePlay__Evolve.rb
+++ scripts/01400 GamePlay/00900 GamePlay__Evolve.rb
@@ -69,14 +69,14 @@ module GamePlay
         if @clone.id == 291 and $actors.size < 6 and $bag.has_item?(4)
           $actors << PFM::Pokemon.new(292)
           $bag.remove_item(4)
-          $pokedex.mark_seen(292)
+          $pokedex.mark_seen(292, forced: true)
           $pokedex.mark_captured(292)
         end
         Audio.bgm_stop
         $game_system.bgm_restore2
         @running = false
         @evolved = true
-        $pokedex.mark_seen(@pokemon.id, @pokemon.form)
+        $pokedex.mark_seen(@pokemon.id, @pokemon.form, forced: true)
         $pokedex.mark_captured(@pokemon.id)
       else
         if(@counter < SecondStep and (!@forced and Input.trigger?(:B)))
