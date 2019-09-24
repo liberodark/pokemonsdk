64a97c2a5b0553923ec5fbebc228822e810a64fb
Nuri Yuri <youri.54@live.fr>
2019-09-24 16:01:11 +0200

    Change the Viewport.create configuration

--- scripts/00000 Dependencies/00400 Patch_Ajouts_LiteRGSS/01000 Viewport.rb
+++ scripts/00000 Dependencies/00400 Patch_Ajouts_LiteRGSS/01000 Viewport.rb
@@ -2,48 +2,78 @@
 #
 # see https://psdk.pokemonworkshop.com/litergss/LiteRGSS/Viewport.html
 class Viewport
-  # Generating a viewport with one line of code
-  # @overload create(x, y = 0, width = 1, height = 1, z = nil)
-  #   @param x [Integer] x coordinate of the viewport
-  #   @param y [Integer] y coordinate of the viewport
-  #   @param width [Integer] width of the viewport
-  #   @param height [Integer] height of the viewport
-  #   @param z [Integer, nil] superiority of the viewport
-  # @overload create(opts)
-  #   @param opts [Hash] opts of the viewport definition
-  #   @option opts [Integer] :x (0) x coordinate of the viewport
-  #   @option opts [Integer] :y (0) y coordinate of the viewport
-  #   @option opts [Integer] :width (320) width of the viewport
-  #   @option opts [Integer] :height (240) height of the viewport
-  #   @option opts [Integer, nil] :z (nil) superiority of the viewport
-  # @overload create(screen_name_symbol, z = nil)
-  #   @param screen_name_symbol [:main, :sub] describe with screen surface the viewport is (loaded from maker options)
-  #   @param z [Integer, nil] superiority of the viewport
-  # @return [Viewport] the generated viewport
-  def self.create(x, y = 0, width = 1, height = 1, z = nil)
-    if x.class == Hash
-      z = x.fetch(:z, nil)
-      y = x.fetch(:y, 0)
-      width = x.fetch(:width, 320)
-      height = x.fetch(:height, 240)
-      x = x.fetch(:x, 0)
-    elsif x == :main || x == :sub
-      z = y
-      if x == :main
-        x = ::Config::Viewport::X
-        y = ::Config::Viewport::Y
-        width = ::Config::Viewport::Width
-        height = ::Config::Viewport::Height
-      else
-        x = ::Config::Viewport::Sub_X
-        y = ::Config::Viewport::Sub_Y
-        width = ::Config::Viewport::Sub_Width
-        height = ::Config::Viewport::Sub_Height
+  # Hash containing all the Viewport configuration (:main, :sub etc...)
+  CONFIGS = {}
+  # Global offset x applied to the viewports (Fullscreen use)
+  GLOBAL_OFFSET_X = 0
+  # Global offset y applied to the viewports (Fullscreen use)
+  GLOBAL_OFFSET_Y = 0
+  # Filename for viewport compiled config
+  VIEWPORT_CONF_COMP = 'Data/Viewport.rxdata'
+  # Filename for viewport uncompiled config
+  VIEWPORT_CONF_TEXT = 'Data/Viewport.json'
+  class << self
+    # Generating a viewport with one line of code
+    # @overload create(x, y = 0, width = 1, height = 1, z = nil)
+    #   @param x [Integer] x coordinate of the viewport
+    #   @param y [Integer] y coordinate of the viewport
+    #   @param width [Integer] width of the viewport
+    #   @param height [Integer] height of the viewport
+    #   @param z [Integer, nil] superiority of the viewport
+    # @overload create(opts)
+    #   @param opts [Hash] opts of the viewport definition
+    #   @option opts [Integer] :x (0) x coordinate of the viewport
+    #   @option opts [Integer] :y (0) y coordinate of the viewport
+    #   @option opts [Integer] :width (320) width of the viewport
+    #   @option opts [Integer] :height (240) height of the viewport
+    #   @option opts [Integer, nil] :z (nil) superiority of the viewport
+    # @overload create(screen_name_symbol, z = nil)
+    #   @param screen_name_symbol [:main, :sub] describe with screen surface the viewport is (loaded from maker options)
+    #   @param z [Integer, nil] superiority of the viewport
+    # @return [Viewport] the generated viewport
+    def create(x, y = 0, width = 1, height = 1, z = 0)
+      if x.is_a?(Hash)
+        z = x[:z] || z
+        y = x[:y] || 0
+        width = x[:width] || Config::ScreenWidth
+        height = x[:height] || Config::ScreenHeight
+        x = x[:x] || 0
+      elsif x.is_a?(Symbol)
+        return create(CONFIGS[x], 0, 1, 1, y)
+      end
+      v = Viewport.new(x + GLOBAL_OFFSET_X, y + GLOBAL_OFFSET_Y, width, height)
+      v.z = z if z
+      return v
+    end
+
+    # Load the viewport configs
+    def load_configs
+      if Config.const_defined?(:Viewport, false) # Old Way
+        CONFIGS.merge!(
+          main: {
+            x: Config::Viewport::X,
+            y: Config::Viewport::Y,
+            width: Config::Viewport::Width,
+            height: Config::Viewport::Height
+          },
+          sub: {
+            x: Config::Viewport::Sub_X,
+            y: Config::Viewport::Sub_Y,
+            width: Config::Viewport::Sub_Width,
+            height: Config::Viewport::Sub_Height
+          }
+        )
+      else # New Way
+        if debug?
+          if !File.exist?(VIEWPORT_CONF_COMP) || File.mtime(VIEWPORT_CONF_TEXT) > File.mtime(VIEWPORT_CONF_COMP)
+            require 'json'
+            log_debug('Updating Viewport Configuration...')
+            save_data(JSON.parse(File.read(VIEWPORT_CONF_TEXT), symbolize_names: true), VIEWPORT_CONF_COMP)
+          end
+        end
+        CONFIGS.merge!(load_data(VIEWPORT_CONF_COMP))
       end
     end
-    v = Viewport.new(x, y, width, height)
-    v.z = z if z
-    return v
   end
 
   # Sort the z sprites inside the viewport
@@ -99,3 +129,5 @@ class Viewport
     end
   end
 end
+
+Graphics.on_start { Viewport.load_configs }
