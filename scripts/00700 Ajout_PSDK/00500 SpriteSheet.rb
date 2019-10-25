0cd931b6a2f6b62dc574ea556fd95aed389934fa
Nuri Yuri <youri.54@live.fr>
2019-10-25 15:03:43 +0200

    Fix SpriteSheet

--- scripts/00700 Ajout_PSDK/00500 SpriteSheet.rb
+++ scripts/00700 Ajout_PSDK/00500 SpriteSheet.rb
@@ -1,7 +1,5 @@
-#encoding: utf-8
-
 # SpriteSheet is a class that helps the maker to display a sprite from a Sprite Sheet on the screen
-class SpriteSheet < ::LiteRGSS::Sprite
+class SpriteSheet < Sprite
   # Return the number of sprite on the x axis of the sheet
   # @return [Integer]
   attr_reader :nb_x
@@ -25,6 +23,7 @@ class SpriteSheet < ::LiteRGSS::Sprite
     @sx = 0
     @sy = 0
   end
+
   # Change the bitmap of the sprite
   # @param value [LiteRGSS::Bitmap, nil]
   def bitmap=(value)
@@ -32,30 +31,33 @@ class SpriteSheet < ::LiteRGSS::Sprite
     if value
       w = value.width /  @nb_x
       h = value.height / @nb_y
-      self.src_rect.set(@sx * w, @sy * h, w, h)
+      src_rect.set(@sx * w, @sy * h, w, h)
     end
     return ret
   end
+
   # Change the x sprite index of the sheet
   # @param value [Integer] the x sprite index of the sheet
   def sx=(value)
     @sx = value % @nb_x
-    self.src_rect.x = @sx * self.src_rect.width
+    src_rect.x = @sx * src_rect.width
   end
+
   # Change the y sprite index of the sheet
   # @param value [Integer] the y sprite index of the sheet
   def sy=(value)
     @sy = value % @nb_y
-    self.src_rect.y = @sy * self.src_rect.height
+    src_rect.y = @sy * src_rect.height
   end
+
   # Select a sprite on the sheet according to its x and y index
   # @param sx [Integer] the x sprite index of the sheet
   # @param sy [Integer] the y sprite index of the sheet
   # @return [self]
   def select(sx, sy)
-    @sx = value % @nb_x
-    @sy = value % @nb_y
-    self.src_rect.set(@sx * self.src_rect.width, @sy * self.src_rect.height, nil, nil)
+    @sx = sx % @nb_x
+    @sy = sy % @nb_y
+    src_rect.set(@sx * src_rect.width, @sy * src_rect.height, nil, nil)
     return self
   end
 end
