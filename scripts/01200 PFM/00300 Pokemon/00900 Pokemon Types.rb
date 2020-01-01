76cc1149f873ee4ae1c430d2d9f98c564e21378e
Nuri Yuri <youri.54@live.fr>
2020-01-01 14:14:49 +0100

    Add GameData::Types

--- scripts/01200 PFM/00300 Pokemon/00900 Pokemon Types.rb
+++ scripts/01200 PFM/00300 Pokemon/00900 Pokemon Types.rb
@@ -21,121 +21,123 @@ module PFM
     # Is the Pokemon type normal ?
     # @return [Boolean]
     def type_normal?
-      return type?(1)
+      return type?(GameData::Types::NORMAL)
     end
 
     # Is the Pokemon type fire ?
     # @return [Boolean]
     def type_fire?
-      return type?(2)
+      return type?(GameData::Types::FIRE)
     end
     alias type_feu? type_fire?
 
     # Is the Pokemon type water ?
     # @return [Boolean]
     def type_water?
-      return type?(3)
+      return type?(GameData::Types::WATER)
     end
     alias type_eau? type_water?
     # Is the Pokemon type electric ?
     # @return [Boolean]
     def type_electric?
-      return type?(4)
+      return type?(GameData::Types::ELECTRIC)
     end
     alias type_electrique? type_electric?
 
     # Is the Pokemon type grass ?
     # @return [Boolean]
     def type_grass?
-      return type?(5)
+      return type?(GameData::Types::GRASS)
     end
     alias type_plante? type_grass?
 
     # Is the Pokemon type ice ?
     # @return [Boolean]
     def type_ice?
-      return type?(6)
+      return type?(GameData::Types::ICE)
     end
     alias type_glace? type_ice?
 
     # Is the Pokemon type fighting ?
     # @return [Boolean]
     def type_fighting?
-      return type?(7)
+      return type?(GameData::Types::FIGHTING)
     end
     alias type_combat? type_fighting?
 
     # Is the Pokemon type poison ?
     # @return [Boolean]
     def type_poison?
-      return type?(8)
+      return type?(GameData::Types::POISON)
     end
 
     # Is the Pokemon type ground ?
     # @return [Boolean]
     def type_ground?
-      return type?(9)
+      return type?(GameData::Types::GROUND)
     end
     alias type_sol? type_ground?
 
     # Is the Pokemon type fly ?
     # @return [Boolean]
-    def type_fly?
-      return type?(10)
+    def type_flying?
+      return type?(GameData::Types::FLYING)
     end
-    alias type_vol? type_fly?
+    alias type_vol? type_flying?
+    alias type_fly? type_flying?
 
     # Is the Pokemon type psy ?
     # @return [Boolean]
     def type_psychic?
-      return type?(11)
+      return type?(GameData::Types::PSYCHIC)
     end
     alias type_psy? type_psychic?
 
     # Is the Pokemon type insect/bug ?
     # @return [Boolean]
-    def type_insect?
-      return type?(12)
+    def type_bug?
+      return type?(GameData::Types::BUG)
     end
+    alias type_insect? type_bug?
 
     # Is the Pokemon type rock ?
     # @return [Boolean]
     def type_rock?
-      return type?(13)
+      return type?(GameData::Types::ROCK)
     end
     alias type_roche? type_rock?
 
     # Is the Pokemon type ghost ?
     # @return [Boolean]
     def type_ghost?
-      return type?(14)
+      return type?(GameData::Types::GHOST)
     end
     alias type_spectre? type_ghost?
 
     # Is the Pokemon type dragon ?
     # @return [Boolean]
     def type_dragon?
-      return type?(15)
+      return type?(GameData::Types::DRAGON)
     end
 
     # Is the Pokemon type steel ?
     # @return [Boolean]
     def type_steel?
-      return type?(16)
+      return type?(GameData::Types::STEEL)
     end
     alias type_acier? type_steel?
 
     # Is the Pokemon type dark ?
     # @return [Boolean]
     def type_dark?
-      return type?(17)
+      return type?(GameData::Types::DARK)
     end
     alias type_tenebre? type_dark?
 
     # Is the Pokemon type fairy ?
     # @return [Boolean]
     def type_fairy?
-      return type?(18)
+      return type?(GameData::Types::FAIRY)
     end
     alias type_fee? type_fairy?
 
