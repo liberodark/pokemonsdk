8348cd17b0baffce7076af0cf2a2960d0807bbc4
Nuri Yuri <nuriyuri@pokemonworkshop.com>
2020-01-19 17:34:17 +0100

    Fix tilemap not being loaded from project_identity

--- scripts/00600 Script_RMXP/03500 Spriteset_Map.rb
+++ scripts/00600 Script_RMXP/03500 Spriteset_Map.rb
@@ -47,9 +47,7 @@ class Spriteset_Map
   # Return the prefered tilemap class
   # @return [Class]
   def tilemap_class
-    return Tilemap::WithLessRubySprites_16 # if ARGV.include?('tilemap')
-    # ((::Config::Yuri_Tilemap_Disabled or $zoom_factor == 2) ? Tilemap : Yuri_Tilemap)
-    # return Yuri_Tilemap
+    return Object.const_get(PSDK_CONFIG.tilemap.tilemap_class)
   end
 
   # Tilemap initialization
