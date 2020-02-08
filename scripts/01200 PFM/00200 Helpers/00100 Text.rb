0b802f73c8573ce4a52a41471133d347452ac350
Nuri Yuri <youri.54@live.fr>
2020-02-08 11:12:56 +0100

    Fix #151

--- scripts/01200 PFM/00200 Helpers/00100 Text.rb
+++ scripts/01200 PFM/00200 Helpers/00100 Text.rb
@@ -226,7 +226,8 @@ module PFM
     # @param text [String] the message
     # @return [String] the parsed message
     def parse_string_for_messages(text)
-      return text if text.empty? # or text.frozen?
+      return text.dup if text.empty? # or text.frozen?
+
       # Detect dialog
       text = detect_dialog(text).dup
       # Gsub text
