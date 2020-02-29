09ac340e2605a864b990c815c2db5ba38f7f73b0
Nuri Yuri <youri.54@live.fr>
2020-02-29 11:33:36 +0100

    Make the gold_window code a bit less cryptic

--- scripts/01100 Yuki/02203 Yuki__Message sub_window.rb
+++ scripts/01100 Yuki/02203 Yuki__Message sub_window.rb
@@ -24,9 +24,9 @@ module Yuki
       return if @gold_window
 
       @gold_window = UI::Window.from_metrics(viewport, 318, 2, 48, 32, position: 'top_right')
-      (stack = @gold_window.sprite_stack).with_surface(0, 0, 44) do
-        stack.add_line(0, text_get(11, 6))
-        stack.add_line(1, PFM::Text.parse(11, 9, ::PFM::Text::NUM7R => $pokemon_party.money.to_s), 2)
+      @gold_window.sprite_stack.with_surface(0, 0, 44) do
+        @gold_window.add_line(0, text_get(11, 6))
+        @gold_window.add_line(1, PFM::Text.parse(11, 9, ::PFM::Text::NUM7R => $pokemon_party.money.to_s), 2)
       end
 
       # Ensure it doesn't shows in the message
