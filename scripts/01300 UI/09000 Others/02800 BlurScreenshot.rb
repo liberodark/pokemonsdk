1666f025b44a68029749247eb20b28fcc51115c2
Nuri Yuri <youri.54@live.fr>
2020-06-14 17:51:26 +0200

    Fix segfault when renaming a caught Pokémon

--- scripts/01300 UI/09000 Others/02800 BlurScreenshot.rb
+++ scripts/01300 UI/09000 Others/02800 BlurScreenshot.rb
@@ -34,6 +34,8 @@ module UI
     # @return [Bitmap]
     def create_snapshot
       bitmap&.dispose
+      return @last_scene.viewport.snap_to_bitmap if @last_scene.is_a?(Scene_Battle)
+
       return @last_scene.snap_to_bitmap
     end
   end
