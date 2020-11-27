b02b0f027a6de5c6c8d3e72287dfc0fbba4be5da
Nuri Yuri <youri.54@live.fr>
2020-11-27 11:30:51 +0100

    Fix accented path issue

--- scripts/tools/GameLoader/1_setupConstantAndLoadPath.rb
+++ scripts/tools/GameLoader/1_setupConstantAndLoadPath.rb
@@ -5,15 +5,15 @@ PSDK_RUNNING_UNDER_WINDOWS = !ENV['windir'].nil?
 PSDK_RUNNING_UNDER_MAC = RUBY_PLATFORM.include? "darwin"
 
 # Constant telling where is the PSDK master installation
-PSDK_PATH = (Dir.exist?('pokemonsdk') && 'pokemonsdk') ||
+PSDK_PATH = (Dir.exist?('pokemonsdk') && File.expand_path('pokemonsdk')) ||
             ((ENV['APPDATA'] || ENV['HOME']).dup.force_encoding('UTF-8') + '/.pokemonsdk')
 
 # Fix $LOAD_PATH
-paths = $LOAD_PATH[0, 10]
-$LOAD_PATH.clear
-$LOAD_PATH.concat(paths.collect { |path| path.dup.force_encoding('UTF-8').freeze })
+# paths = $LOAD_PATH[0, 10]
+# $LOAD_PATH.clear
+# $LOAD_PATH.concat(paths.collect { |path| path.dup.force_encoding('UTF-8').freeze })
 # Add . and ./plugins to load_path
-$LOAD_PATH << '.' unless $LOAD_PATH.include?('.')
+# $LOAD_PATH << '.' unless $LOAD_PATH.include?('.')
 $LOAD_PATH << './plugins' unless $LOAD_PATH.include?('./plugins')
 
 ENV['SSL_CERT_FILE'] ||= './lib/cert.pem' if $0 == 'Game.rb' # Launched from PSDK
