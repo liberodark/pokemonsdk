b106ad349eb58ddfcdd7cdf9ff867aff4891cbe1
Aerun <nicolasdoll54@gmail.com>
2021-02-08 00:15:15 +0100

    Implement HP Eq Level moves

---
+++ scripts/01600 Alpha 25 Battle Engine/04160 SpecialMoves/00309 HPEqLevel.rb
@@ -0,0 +1,22 @@
+module Battle
+  class Move
+    # Class managing moves that deal damages equivalent level
+    class HPEqLevel < Basic
+      private
+
+      # Method calculating the damages done by the actual move
+      # @param user [PFM::PokemonBattler] user of the move
+      # @param target [PFM::PokemonBattler] target of the move
+      # @param rng [Random] random generator used for the move
+      # @return [Integer]
+      def damages(user, target, rng)
+        @critical = false
+        @effectiveness = 1
+        log_data("Damages equivalent to the user Level Move: #{user.level} HP")
+        return user.level || 1
+      end
+    end
+
+    Move.register(:s_hp_eq_level, HPEqLevel)
+  end
+end
