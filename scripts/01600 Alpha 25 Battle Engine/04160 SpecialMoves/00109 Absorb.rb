27d3a6a8ee0abf35fac38650ca40af3167a31ea0
Aerun <nicolasdoll54@gmail.com>
2021-02-09 00:43:18 +0100

    Implement Dream Eater

--- scripts/01600 Alpha 25 Battle Engine/04160 SpecialMoves/00109 Absorb.rb
+++ scripts/01600 Alpha 25 Battle Engine/04160 SpecialMoves/00109 Absorb.rb
@@ -4,6 +4,16 @@ module Battle
     class Absorb < Move
       private
 
+      # Test if the target is immune
+      # @param user [PFM::PokemonBattler]
+      # @param target [PFM::PokemonBattler]
+      # @return [Boolean]
+      def target_immune?(user, target)
+        return true if !target.asleep? && be_method == :s_dream_eater
+
+        return super
+      end
+
       # Function that deals the damage to the pokemon
       # @param user [PFM::PokemonBattler] user of the move
       # @param actual_targets [Array<PFM::PokemonBattler>] targets that will be affected by the move
@@ -34,14 +44,13 @@ module Battle
       # @note Thing that prevents the move from being used should be defined by :move_prevention_user Hook
       # @return [Boolean] if the procedure can continue
       def move_usable_by_user(user, targets)
-        return true if super
-
         if user.effects.has?(:heal_block)
-          scene.display_message_and_wait(parse_text_with_pokemon(19, 893, target,
-                                                                 '[VAR PKNICK(0000)]' => target.given_name,
+          scene.display_message_and_wait(parse_text_with_pokemon(19, 893, user,
+                                                                 '[VAR PKNICK(0000)]' => user.given_name,
                                                                  '[VAR MOVE(0001)]' => name))
           return false
         end
+        return true if super
       end
 
       # Tell that the move is a drain move
@@ -52,5 +61,6 @@ module Battle
     end
 
     Move.register(:s_absorb, Absorb)
+    Move.register(:s_dream_eater, Absorb)
   end
 end
