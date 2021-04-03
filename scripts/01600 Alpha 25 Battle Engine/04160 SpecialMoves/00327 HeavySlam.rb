6c719e2bb1199f61437552f27508f339a0a8b5fc
Nuri Yuri <youri.54@live.fr>
2021-04-03 17:57:49 +0200

    Implement HeavySlam & LowKick, fix stomp

--- scripts/01600 Alpha 25 Battle Engine/04160 SpecialMoves/00327 HeavySlam.rb
+++ scripts/01600 Alpha 25 Battle Engine/04160 SpecialMoves/00327 HeavySlam.rb
@@ -1,14 +1,16 @@
 module Battle
   class Move
-    class Stomp < Basic
+    class HeavySlam < Basic
+      MINIMUM_WEIGHT_PERCENT = [0.5, 0.3334, 0.25, 0.20]
       # Get the real base power of the move (taking in account all parameter)
       # @param user [PFM::PokemonBattler] user of the move
       # @param target [PFM::PokemonBattler] target of the move
       # @return [Integer]
       def real_base_power(user, target)
-        return super * 2 if target.effects.has?(:minimize)
-
-        return super
+        weight_percent = target.weight.to_f / user.weight
+        weight_index = MINIMUM_WEIGHT_PERCENT.find_index { |weight| weight_percent > weight } || MINIMUM_WEIGHT_PERCENT.size
+        minimize_factor = target.effects.has?(:minimize) ? 2 : 1
+        return (40 + 20 * weight_index) * minimize_factor
       end
 
       # Return the chance of hit of the move
@@ -20,6 +22,6 @@ module Battle
       end
     end
 
-    Move.register(:s_stomp, Stomp)
+    Move.register(:s_heavy_slam, HeavySlam)
   end
 end
