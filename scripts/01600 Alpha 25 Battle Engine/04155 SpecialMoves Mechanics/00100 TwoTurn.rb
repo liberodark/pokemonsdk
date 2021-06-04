d44ea5c98c92e3bdefa62e5833ad8908fcf9e59c
Leikt <leikt.solreihin@gmail.com>
2021-06-04 07:50:34 +0200

    Fix: 2turn moves continue through paralysis

--- scripts/01600 Alpha 25 Battle Engine/04155 SpecialMoves Mechanics/00100 TwoTurn.rb
+++ scripts/01600 Alpha 25 Battle Engine/04155 SpecialMoves Mechanics/00100 TwoTurn.rb
@@ -25,6 +25,7 @@ module Battle
         # @note Thing that prevents the move from being used should be defined by :move_prevention_user Hook
         # @return [Boolean] if the procedure can continue
         def move_usable_by_user_turn1(super_result, user, targets)
+          return false unless super_result
           return true if check_shortcut_turn1(user, targets)
 
           proceed_effects_turn1(user, targets)
