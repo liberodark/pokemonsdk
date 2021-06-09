928597642743b478dcaeeab591127e11280203b6
Aerun <2930148-Aerun@users.noreply.gitlab.com>
2021-06-09 23:36:17 +0000

    Fix parse_text method

--- scripts/01600 Alpha 25 Battle Engine/00250 Effects/00700 Ability Effects/00100 Unnerve.rb
+++ scripts/01600 Alpha 25 Battle Engine/00250 Effects/00700 Ability Effects/00100 Unnerve.rb
@@ -10,7 +10,7 @@ module Battle
           return if with != @target
 
           handler.scene.visual.show_ability(with)
-          handler.scene.display_message_and_wait(parse_text_with_pokemon(18, with.bank == 0 ? 183 : 182))
+          handler.scene.display_message_and_wait(parse_text(18, with.bank == 0 ? 183 : 182))
         end
       end
       register(:unnerve, Unnerve)
