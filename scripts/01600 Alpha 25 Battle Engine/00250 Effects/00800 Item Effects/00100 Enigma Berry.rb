3e7f2243f529efa709fe751f90069a4518ef34f7
Nuri Yuri <youri.54@live.fr>
2021-06-18 16:51:06 +0200

    Implement enigma_berry, jaboca_berry, rowap_berry

---
+++ scripts/01600 Alpha 25 Battle Engine/00250 Effects/00800 Item Effects/00100 Enigma Berry.rb
@@ -0,0 +1,56 @@
+module Battle
+  module Effects
+    class Item
+      class EnigmaBerry < Berry
+        # Function called after damages were applied (post_damage, when target is still alive)
+        # @param handler [Battle::Logic::DamageHandler]
+        # @param hp [Integer] number of hp (damage) dealt
+        # @param target [PFM::PokemonBattler]
+        # @param launcher [PFM::PokemonBattler, nil] Potential launcher of a move
+        # @param skill [Battle::Move, nil] Potential move used
+        def on_post_damage(handler, hp, target, launcher, skill)
+          return if target != @target
+          return if skill&.be_method == :s_thief && launcher&.item_db_symbol == :__undef__
+
+          process_effect(target, launcher, skill)
+        end
+
+        # Function that executes the effect of the berry (for Pluck & Bug Bite)
+        def execute_berry_effect
+          # Remove the following line if the berry should be executed only if the condition match
+          define_singleton_method(:trigger?) { |_| true }
+          process_effect(@target, nil, nil)
+        end
+
+        private
+
+        # Function that process the effect of the berry (if possible)
+        # @param target [PFM::PokemonBattler]
+        # @param launcher [PFM::PokemonBattler, nil] Potential launcher of a move
+        # @param skill [Battle::Move, nil] Potential move used
+        def process_effect(target, launcher, skill)
+          return if cannot_be_consumed? || !trigger?(skill)
+
+          item_name = target.item_name
+          consume_berry(target, launcher, skill)
+          @logic.scene.visual.show_hp_animations([target], [hp_healed])
+          @logic.scene.display_message_and_wait(parse_text_with_pokemon(19, 914, target, PFM::Text::ITEM2[1] => item_name))
+        end
+
+        # Tell if the berry triggers
+        # @param move [Battle::Move, nil] Potential move used
+        # @return [Boolean]
+        def trigger?(move)
+          return move&.super_effective?
+        end
+
+        # Give the amount of HP healed
+        # @return [Integer]
+        def hp_healed
+          return (@target.max_hp / 4).clamp(1, Float::INFINITY)
+        end
+      end
+      register(:enigma_berry, EnigmaBerry)
+    end
+  end
+end
