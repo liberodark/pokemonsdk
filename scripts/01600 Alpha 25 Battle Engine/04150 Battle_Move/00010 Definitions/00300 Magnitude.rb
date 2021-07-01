a3e6186a0b4870f3e223bbb5ab30b13f55cc1b59
Palbolsky <rodolphe0904@wanadoo.fr>
2021-07-01 22:57:16 +0000

    Fix Magnitude move

--- scripts/01600 Alpha 25 Battle Engine/04150 Battle_Move/00010 Definitions/00300 Magnitude.rb
+++ scripts/01600 Alpha 25 Battle Engine/04150 Battle_Move/00010 Definitions/00300 Magnitude.rb
@@ -10,7 +10,11 @@ module Battle
       # @param target [PFM::PokemonBattler] target of the move
       # @return [Integer]
       def real_base_power(user, target)
-        return @magnitude_power
+        return magnitude_table[3][1] unless @magnitude_found # magnitude_found = nil when real_base_power called by AI
+        log_data("magnitude power #{@magnitude_found[1]} # #{@magnitude_found}")
+        power = @magnitude_found[1]
+        @magnitude_found = nil
+        return power
       end
 
       # Method calculating the damages done by the actual move
@@ -28,16 +32,24 @@ module Battle
 
       private
 
+      # Function that tests if the user is able to use the move
+      # @param user [PFM::PokemonBattler] user of the move
+      # @param targets [Array<PFM::PokemonBattler>] expected targets
+      # @note Thing that prevents the move from being used should be defined by :move_prevention_user Hook
+      # @return [Boolean] if the procedure can continue
+      def move_usable_by_user(user, targets)
+        return false unless super
+        # Pick a random magnitude data
+        dice = logic.generic_rng.rand(100).floor
+        @magnitude_found = magnitude_table.find { |row| row[0] > dice } || magnitude_table[0]
+        return true
+      end
+
       # Show the move usage message
       # @param user [PFM::PokemonBattler] user of the move
       def usage_message(user)
         super
-        # Pick a random magnitude data
-        dice = logic.generic_rng.rand(100).floor
-        magnitude_data = magnitude_table.find { |row| row[0] > dice } || magnitude_table[0]
-        @magnitude_power = magnitude_data[1]
-        log_data("magnitude power #{@magnitude_power} # #{magnitude_data}")
-        @scene.display_message_and_wait(parse_text(18, magnitude_data[2]))
+        @scene.display_message_and_wait(parse_text(18, @magnitude_found[2]))
       end
 
       # Damage table
