f85cff74c95b130c01390ac7426e5a28bdc81aea
Nuri Yuri <youri.54@live.fr>
2021-07-18 12:15:15 +0200

    Ensure that AI don't attempt to mega evolve if it wants to switch

--- scripts/01600 Alpha 25 Battle Engine/08000 Battle_AI/00100 Base.rb
+++ scripts/01600 Alpha 25 Battle Engine/08000 Battle_AI/00100 Base.rb
@@ -69,6 +69,7 @@ module Battle
 
         exec_hooks(Base, :battle_action_for, binding)
         final_action = actions.compact.shuffle(random: @scene.logic.generic_rng).max_by(&:first)&.last
+        mega = nil if final_action.is_a?(Actions::Switch)
 
         return mega ? [mega, final_action] : final_action
       end
