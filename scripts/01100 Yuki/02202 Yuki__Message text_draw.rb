ab4ee0da16566b4adfcc47b1a370abb21ab24e5d
Palbolsky <palbolsky@gmail.com>
2021-08-27 15:52:21 +0200

    Fix no new line in message if csv and \nl\c[x]

--- scripts/01100 Yuki/02202 Yuki__Message text_draw.rb
+++ scripts/01100 Yuki/02202 Yuki__Message text_draw.rb
@@ -65,7 +65,13 @@ module Yuki
         return x if text.empty?
         return (arr << :new_line; x) if text == S_n
 
-        text.split(S_n).each_with_index do |line, i|
+        texts = text.split(S_n)
+        if texts.size == 1 && text[-1] == S_n
+          adjust_text_lines(x, max_width, texts[0], arr, true)
+          return (arr << :new_line; x = 0)
+        end
+
+        texts.each_with_index do |line, i|
           (arr << :new_line; x = 0) if i > 0
           x = adjust_text_lines(x, max_width, line, arr, true)
         end
