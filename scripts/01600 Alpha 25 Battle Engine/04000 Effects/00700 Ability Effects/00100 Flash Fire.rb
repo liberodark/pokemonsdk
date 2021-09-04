08b24db17c38fa493f1624ae007f2ca941b67f47
Aerun <2930148-Aerun@users.noreply.gitlab.com>
2021-09-04 14:21:02 +0000

    Fix FlashFire

--- scripts/01600 Alpha 25 Battle Engine/04000 Effects/00700 Ability Effects/00100 Flash Fire.rb
+++ scripts/01600 Alpha 25 Battle Engine/04000 Effects/00700 Ability Effects/00100 Flash Fire.rb
@@ -15,9 +15,61 @@ module Battle
           return unless launcher&.can_be_lowered_or_canceled?
 
           return handler.prevent_change do
-            handler.scene.visual.show_ability(target) if target.frozen? || !@boost_enabled
             handler.logic.status_change_handler.status_change_with_process(:cure, target) if target.frozen?
+            handler.scene.visual.show_ability(target)
+            handler.scene.display_message_and_wait(parse_text_with_pokemon(19, @boost_enabled ? 24 : 427, target))
             @boost_enabled = true
+          end
+        end
+
+        # Function called when a stat_decrease_prevention is checked
+        # @param handler [Battle::Logic::StatChangeHandler] handler use to test prevention
+        # @param stat [Symbol] :atk, :dfe, :spd, :ats, :dfs, :acc, :eva
+        # @param target [PFM::PokemonBattler]
+        # @param launcher [PFM::PokemonBattler, nil] Potential launcher of a move
+        # @param skill [Battle::Move, nil] Potential move used
+        # @return [:prevent, nil] :prevent if the stat decrease cannot apply
+        def on_stat_decrease_prevention(handler, stat, target, launcher, skill)
+          return if target != @target
+          return unless skill&.type_fire?
+          return unless launcher && target != launcher && launcher.can_be_lowered_or_canceled?
+
+          return :prevent
+        end
+
+        # Function called when a stat_increase_prevention is checked
+        # @param handler [Battle::Logic::StatChangeHandler] handler use to test prevention
+        # @param stat [Symbol] :atk, :dfe, :spd, :ats, :dfs, :acc, :eva
+        # @param target [PFM::PokemonBattler]
+        # @param launcher [PFM::PokemonBattler, nil] Potential launcher of a move
+        # @param skill [Battle::Move, nil] Potential move used
+        # @return [:prevent, nil] :prevent if the stat increase cannot apply
+        def on_stat_increase_prevention(handler, stat, target, launcher, skill)
+          return if target != @target
+          return unless skill&.type_fire?
+          return unless launcher && target != launcher && launcher.can_be_lowered_or_canceled?
+
+          return :prevent
+        end
+
+        # Function called when a status_prevention is checked
+        # @param handler [Battle::Logic::StatusChangeHandler]
+        # @param status [Symbol] :poison, :toxic, :confusion, :sleep, :freeze, :paralysis, :burn, :flinch, :cure
+        # @param target [PFM::PokemonBattler]
+        # @param launcher [PFM::PokemonBattler, nil] Potential launcher of a move
+        # @param skill [Battle::Move, nil] Potential move used
+        # @return [:prevent, nil] :prevent if the status cannot be applied
+        def on_status_prevention(handler, status, target, launcher, skill)
+          return if target != @target
+          return unless status == :burn
+          return unless launcher&.can_be_lowered_or_canceled?
+
+          return handler.prevent_change do
+            if skill.status?
+              handler.scene.visual.show_ability(target)
+              handler.scene.display_message_and_wait(parse_text_with_pokemon(19, @boost_enabled ? 24 : 427, target))
+              @boost_enabled = true
+            end
           end
         end
 
@@ -29,7 +81,7 @@ module Battle
         def mod1_multiplier(user, target, move)
           return 1 if user != @target || !@boost_enabled
 
-          return move.type_fire? ? 1.5 : 0
+          return move.type_fire? ? 1.5 : 1
         end
 
         # Reset the boost when leaving battle
