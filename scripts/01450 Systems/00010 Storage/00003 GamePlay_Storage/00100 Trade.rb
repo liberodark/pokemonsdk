f47cbbeadfdf7fa740f7ead061fb07108648ae64
Aerun <2930148-Aerun@users.noreply.gitlab.com>
2021-09-30 01:09:51 +0000

    Translate the pokemon storage system

--- scripts/01450 Systems/00010 Storage/00003 GamePlay_Storage/00100 Trade.rb
+++ scripts/01450 Systems/00010 Storage/00003 GamePlay_Storage/00100 Trade.rb
@@ -1,7 +1,7 @@
 module GamePlay
   class PokemonTradeStorage < PokemonStorage
     # Message shown to tell to choose a Pokemon
-    CHOOSE_POKEMON_MESSAGE = 'Choose a Pokemon to trade'
+    CHOOSE_POKEMON_MESSAGE = [:ext_text, 9007, 13] # "Choose a Pokemon to trade."
     # List of option when pressing A
     TRADE_OPTIONS = [
       [:ext_text, 9000, 90], # Trade
@@ -56,11 +56,11 @@ module GamePlay
       play_decision_se
       choices = PFM::Choice_Helper.new(Yuki::ChoiceWindow::But, true, 999)
       choices
-        .register_choice(get_text(TRADE_OPTIONS[0]), on_validate: method(:trade_pokemon))
-        .register_choice(get_text(TRADE_OPTIONS[1]), on_validate: method(:show_pokemon_summary))
-        .register_choice(get_text(TRADE_OPTIONS[2]))
+        .register_choice(send(*TRADE_OPTIONS[0]), on_validate: method(:trade_pokemon))
+        .register_choice(send(*TRADE_OPTIONS[1]), on_validate: method(:show_pokemon_summary))
+        .register_choice(send(*TRADE_OPTIONS[2]))
 
-      @base_ui.show_win_text(format(get_text(SINGLE_POKEMON_MESSAGE), name: @current_pokemon.given_name))
+      @base_ui.show_win_text(format(send(*SINGLE_POKEMON_MESSAGE), name: @current_pokemon.given_name))
       @base_ui.mode = 7
       choice = choices.display_choice(@viewport, *choice_coordinates(choices), nil, on_update: method(:update_graphics), align_right: true)
       show_pokemon_choice if choice != 0
