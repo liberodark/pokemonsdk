870c05fdca60ea38881e1c603cf7d5342b48ecc9
Aerun <nicolasdoll54@gmail.com>
2022-01-09 01:53:06 +0100

    Fix U Turn soft lock

--- scripts/01600 Alpha 25 Battle Engine/00200 Battle_Logic/00001 Handlers/01005 SwitchHandler.rb
+++ scripts/01600 Alpha 25 Battle Engine/00200 Battle_Logic/00001 Handlers/01005 SwitchHandler.rb
@@ -136,14 +136,24 @@ module Battle
     end
 
     # U-Turn moves
-    SwitchHandler.register_switch_passthrough_hook('PSDK switch pass: U-Turn moves') do |_, _, skill|
-      next :passthrough if skill&.self_user_switch?
+    SwitchHandler.register_switch_prevention_hook('PSDK switch prevent: U-Turn moves') do |handler, pokemon, skill|
+      party = handler.logic.all_battlers.select { |p| p.bank == pokemon.bank && p.party_id == pokemon.party_id }
+      next :prevent if (party - [pokemon] - handler.logic.allies_of(pokemon)).count(&:alive?) == 0 && skill&.self_user_switch?
+    end
+
+    # U-Turn moves
+    SwitchHandler.register_switch_passthrough_hook('PSDK switch pass: U-Turn moves') do |handler, pokemon, skill|
+      party = handler.logic.all_battlers.select { |p| p.bank == pokemon.bank && p.party_id == pokemon.party_id }
+      alive_pokemon = (party - [pokemon] - handler.logic.allies_of(pokemon)).count(&:alive?)
+
+      next :passthrough if skill&.self_user_switch? && alive_pokemon > 0
     end
 
     # Encounter list
     SwitchHandler.register_switch_event_hook('Update encounter list') do |handler, _, with|
       handler.logic.all_battlers do |battler|
         next if battler.position == -1 || battler.dead? || battler == with
+
         battler.add_battler_to_encounter_list(with)
         with.add_battler_to_encounter_list(battler)
       end
