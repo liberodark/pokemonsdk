5c90572031a340e2da36ab156e0aa5db02167c4e
Aerun <2930148-Aerun@users.noreply.gitlab.com>
2022-02-02 21:58:44 +0000

    Fix request_switch_to_trainer

--- scripts/01600 Alpha 25 Battle Engine/00200 Battle_Logic/00104 end of battle phase & switch choice.rb
+++ scripts/01600 Alpha 25 Battle Engine/00200 Battle_Logic/00104 end of battle phase & switch choice.rb
@@ -51,8 +51,8 @@ module Battle
       log_data("Pokemon switched with #{who} : #{with}")
       return unless with
 
-      during_end_of_turn = @actions.empty? && !@scene.force_ia_switch?
-      request_switch_to_trainer(with) if who.bank != 0 && who.dead? && during_end_of_turn
+      during_end_of_turn = @actions.empty?
+      request_switch_to_trainer(with) if who.bank != 0 && who.dead? && during_end_of_turn && @scene.force_ia_switch?
       Actions::Switch.new(@scene, who, with).execute
     end
 
