9e796b4014fdd6d18216de4ffc234f8ca3869d7e
Aerun <2930148-Aerun@users.noreply.gitlab.com>
2022-02-02 22:22:03 +0000

    Update play_animation method to include $options.show_animation check

--- scripts/01600 Alpha 25 Battle Engine/04150 Battle_Move/00120 Procedure.rb
+++ scripts/01600 Alpha 25 Battle Engine/04150 Battle_Move/00120 Procedure.rb
@@ -220,6 +220,8 @@ module Battle
     # @param user [PFM::PokemonBattler] user of the move
     # @param targets [Array<PFM::PokemonBattler>] expected targets
     def play_animation(user, targets)
+      return unless $options.show_animation
+
       @scene.visual.set_info_state(:move_animation)
       @scene.visual.wait_for_animation
       play_animation_internal(user, targets)
