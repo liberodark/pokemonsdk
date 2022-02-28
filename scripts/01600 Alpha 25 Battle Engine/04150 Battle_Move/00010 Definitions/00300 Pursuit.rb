6780bc6bc415f1de3fbabf668ac2d44f8d0ac3b8
Aerun <nicolasdoll54@gmail.com>
2022-02-28 16:34:49 +0100

    Fix Pursuit - Now reject the switch requests if the Pokémon is KO

--- scripts/01600 Alpha 25 Battle Engine/04150 Battle_Move/00010 Definitions/00300 Pursuit.rb
+++ scripts/01600 Alpha 25 Battle Engine/04150 Battle_Move/00010 Definitions/00300 Pursuit.rb
@@ -1,7 +1,7 @@
 module Battle
   class Move
     # Pursuit move, double the power if hitting switching out Pokemon
-    class Pursuit < Basic
+    class Pursuit < BasicWithSuccessfulEffect
       # Get the real base power of the move (taking in account all parameter)
       # @param user [PFM::PokemonBattler] user of the move
       # @param target [PFM::PokemonBattler] target of the move
@@ -10,6 +10,16 @@ module Battle
         return super * 2 if target.switching? && target.last_sent_turn != $game_temp.battle_turn
 
         return super
+      end
+
+      private
+
+      # Function that deals the effect to the pokemon
+      # @param user [PFM::PokemonBattler] user of the move
+      # @param actual_targets [Array<PFM::PokemonBattler>] targets that will be affected by the move
+      def deal_effect(user, actual_targets)
+        @logic.actions.reject! { |a| a.is_a?(Actions::Switch) && actual_targets.include?(a.who) && a.who.dead? }
+        return true
       end
     end
 
