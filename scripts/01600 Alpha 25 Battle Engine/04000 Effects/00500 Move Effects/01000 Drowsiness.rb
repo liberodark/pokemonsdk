797644269afb4fda6823e7f2c4f8eaba9b888a3a
Aerun <nicolasdoll54@gmail.com>
2022-03-01 13:41:30 +0100

    Fix Yawn & Drowsiness

--- scripts/01600 Alpha 25 Battle Engine/04000 Effects/00500 Move Effects/01000 Drowsiness.rb
+++ scripts/01600 Alpha 25 Battle Engine/04000 Effects/00500 Move Effects/01000 Drowsiness.rb
@@ -18,13 +18,13 @@ module Battle
       def triggered?
         return @counter == 1
       end
-      
+
       # Get the name of the effect
       # @return [Symbol]
       def name
         return :drowsiness
       end
-      
+
       # Function called at the end of a turn
       # @param logic [Battle::Logic] logic of the battle
       # @param scene [Battle::Scene] battle scene
@@ -32,8 +32,18 @@ module Battle
       def on_end_turn_event(logic, scene, battlers)
         return if @pokemon.dead?
         return unless triggered?
+        return kill if logic.terrain_effects.has?(:electric_terrain) && @pokemon.grounded?
+        return kill if @pokemon.asleep?
 
         logic.status_change_handler.status_change_with_process(:sleep, @pokemon)
+      end
+
+      # Function called when a Pokemon has actually switched with another one
+      # @param handler [Battle::Logic::SwitchHandler]
+      # @param who [PFM::PokemonBattler] Pokemon that is switched out
+      # @param with [PFM::PokemonBattler] Pokemon that is switched in
+      def on_switch_event(handler, who, with)
+        kill if who == @pokemon
       end
     end
   end
--- scripts/01600 Alpha 25 Battle Engine/04150 Battle_Move/00010 Definitions/00300 Yawn.rb
+++ scripts/01600 Alpha 25 Battle Engine/04150 Battle_Move/00010 Definitions/00300 Yawn.rb
@@ -13,6 +13,23 @@ module Battle
         return true
       end
 
+      # Function that tests if the user is able to use the move
+      # @param user [PFM::PokemonBattler] user of the move
+      # @param targets [Array<PFM::PokemonBattler>] expected targets
+      # @note Thing that prevents the move from being used should be defined by :move_prevention_user Hook
+      # @return [Boolean] if the procedure can continue
+      def move_usable_by_user(user, targets)
+        return false unless super
+
+        if targets.all? { |target| target.effects.has?(:drowsiness) || target.effects.has?(:substitute) } ||
+           @logic.terrain_effects.has?(:electric_terrain) && targets.all?(&:grounded?)
+          show_usage_failure(user)
+          return false
+        end
+
+        return true
+      end
+
       # Function that tests if the targets blocks the move
       # @param user [PFM::PokemonBattler] user of the move
       # @param target [PFM::PokemonBattler] expected target
@@ -20,6 +37,8 @@ module Battle
       # @return [Boolean] if the target evade the move (and is not selected)
       def move_blocked_by_target?(user, target)
         return true if super
+        return failure_message if target.effects.has?(:drowsiness) || target.effects.has?(:substitute)
+        return failure_message if @logic.terrain_effects.has?(:electric_terrain) && target.grounded?
         return true unless logic.status_change_handler.status_appliable?(:sleep, target, user)
 
         return false
@@ -39,6 +58,13 @@ module Battle
       # @return [Integer]
       def turn_count
         2
+      end
+
+      # Display failure message
+      # @return [Boolean] true if blocked
+      def failure_message
+        @logic.scene.display_message_and_wait(parse_text(18, 74))
+        return true
       end
     end
     Move.register(:s_yawn, Yawn)
