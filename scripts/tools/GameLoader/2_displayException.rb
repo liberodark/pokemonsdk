a8653ed434b443a6988d4c435c315df1872b4ba2
Scorbutics <brice.bulgarelli@gmail.com>
2022-03-11 16:22:07 +0100

    #332 feature: exception logging on STDERR

--- scripts/tools/GameLoader/2_displayException.rb
+++ scripts/tools/GameLoader/2_displayException.rb
@@ -1,10 +1,10 @@
 # Function that display an exception with a message and clean up
 # @param message [String] message for the exception
 def display_game_exception(message)
-  puts message
-  puts format('Error type : %<class>s', class: $!.class)
-  puts format('Error message : %<message>s', message: $!.message)
-  puts $!.backtrace
+  STDERR.puts message
+  STDERR.puts format('Error type : %<class>s', class: $!.class)
+  STDERR.puts format('Error message : %<message>s', message: $!.message)
+  STDERR.puts $!.backtrace
   Audio.bgm_stop rescue nil
   Audio.bgs_stop rescue nil
   Audio.me_stop rescue nil
