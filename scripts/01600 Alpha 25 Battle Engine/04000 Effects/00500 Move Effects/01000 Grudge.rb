68d92dfbd16252418aef096acbc0fe668db4853a
Aerun <2930148-Aerun@users.noreply.gitlab.com>
2022-04-18 06:46:54 +0000

    Fix Secret Power and Grudge

--- scripts/01600 Alpha 25 Battle Engine/04000 Effects/00500 Move Effects/01000 Grudge.rb
+++ scripts/01600 Alpha 25 Battle Engine/04000 Effects/00500 Move Effects/01000 Grudge.rb
@@ -10,11 +10,20 @@ module Battle
       # @param skill [Battle::Move, nil] Potential move used
       def on_post_damage_death(handler, hp, target, launcher, skill)
         return if target != @pokemon
+        return unless skill && launcher
 
         if skill.direct? && target.move_history.last.move.be_method == :s_grudge
           handler.scene.display_message_and_wait(parse_text_with_pokemon(19, 635, launcher, ::PFM::Text::MOVE[1] => skill.name))
           skill.pp = 0
         end
+      end
+
+      # Function called when a Pokemon has actually switched with another one
+      # @param handler [Battle::Logic::SwitchHandler]
+      # @param who [PFM::PokemonBattler] Pokemon that is switched out
+      # @param with [PFM::PokemonBattler] Pokemon that is switched in
+      def on_switch_event(handler, who, with)
+        kill if who == @pokemon
       end
 
       # Get the name of the effect
