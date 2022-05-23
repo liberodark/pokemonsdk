80f737cfc0efa78b9d0a4b6d8f0c89b68c2bd457
Aerun <nicolasdoll54@gmail.com>
2022-05-23 12:30:08 +0200

    Fix method names for PPIncreaseItem

--- scripts/00800 Studio/00001 Data/00002 Item/00300 PPIncreaseItem.rb
+++ scripts/00800 Studio/00001 Data/00002 Item/00300 PPIncreaseItem.rb
@@ -15,19 +15,19 @@ PFM::ItemDescriptor.define_on_creature_usability(Studio::PPIncreaseItem) do |_, 
   next false if creature.egg?
 
   moves = $game_temp.in_battle ? PFM::PokemonBattler.from(creature).moveset : creature.skills_set
-  next moves.any? { |move| (move.data.pp_max * 8 / 5) > move.ppmax }
+  next moves.any? { |move| (move.data.pp * 8 / 5) > move.ppmax }
 end
 
 PFM::ItemDescriptor.define_on_move_usability(Studio::PPIncreaseItem, 35) do |_, skill|
-  next (skill.data.pp_max * 8 / 5) > skill.ppmax
+  next (skill.data.pp * 8 / 5) > skill.ppmax
 end
 
 PFM::ItemDescriptor.define_on_move_use(Studio::PPIncreaseItem) do |item, creature, skill, scene|
   creature.loyalty -= Studio::HealingItem.from(item).loyalty_malus
   if Studio::PPIncreaseItem.from(item).is_max
-    skill.ppmax = skill.data.pp_max * 8 / 5
+    skill.ppmax = skill.data.pp * 8 / 5
   else
-    skill.ppmax += skill.data.pp_max * 1 / 5
+    skill.ppmax += skill.data.pp * 1 / 5
   end
   skill.pp += 99
   scene.display_message_and_wait(parse_text(22, 117, PFM::Text::MOVE[0] => skill.name))
