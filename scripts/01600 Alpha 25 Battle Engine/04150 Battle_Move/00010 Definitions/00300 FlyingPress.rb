a8cdf3179ce9d066740c2959a712273b11bdb5f9
Rey <reynald-leroy@hotmail.fr>
2022-07-06 23:04:17 +0200

    Fix Flying Press and add a second_type method for easier monkey-patching + add things related to Minimize

--- scripts/01600 Alpha 25 Battle Engine/04150 Battle_Move/00010 Definitions/00300 FlyingPress.rb
+++ scripts/01600 Alpha 25 Battle Engine/04150 Battle_Move/00010 Definitions/00300 FlyingPress.rb
@@ -1,13 +1,39 @@
 module Battle
   class Move
     # Move that has a flying type as second type
-    class FlyingPress < Move
+    class FlyingPress < Basic
       # Get the types of the move with 1st type being affected by effects
       # @param user [PFM::PokemonBattler] user of the move
       # @param target [PFM::PokemonBattler] target of the move
       # @return [Array<Integer>] list of types of the move
       def definitive_types(user, target)
-        super << data_type(:flying).id
+        super << data_type(second_type).id
+      end
+
+      # Method calculating the damages done by the actual move
+      # @param user [PFM::PokemonBattler] user of the move
+      # @param target [PFM::PokemonBattler] target of the move
+      # @return [Integer]
+      def damages(user, target)
+        return target.effects.has?(:minimize) ? super * 2 : super
+      end
+
+      # Check if the move bypass chance of hit and cannot fail
+      # @param user [PFM::PokemonBattler] user of the move
+      # @param target [PFM::PokemonBattler] target of the move
+      # @return [Boolean]
+      def bypass_chance_of_hit?(user, target)
+        return true if target.effects.has?(:minimize)
+
+        super
+      end
+
+      private
+
+      # Get the second type of the move
+      # @return [Symbol]
+      def second_type
+        return :flying
       end
     end
 
