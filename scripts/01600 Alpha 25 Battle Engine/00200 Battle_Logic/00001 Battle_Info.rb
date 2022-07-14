09d50c867105d8108994a1d1ca04c7002f4ab2ab
Rey <3939415-TheRey@users.noreply.gitlab.com>
2022-07-14 15:43:24 +0000

    Invert bag and base_money parameters for method BattleInfo#add_trainer

--- scripts/01600 Alpha 25 Battle Engine/00200 Battle_Logic/00001 Battle_Info.rb
+++ scripts/01600 Alpha 25 Battle Engine/00200 Battle_Logic/00001 Battle_Info.rb
@@ -115,8 +115,8 @@ module Battle
           name = trainer.name
           party = trainer.party.map(&:to_creature)
           bag = PFM::Bag.new
-          trainer.bag_entries.each { |bag_entry| bag_entry.each { |key, value| bag.add_item(key, value )} }
-          battle_info.add_party(bank, party, name, klass, battler, nil, bag, trainer.ai)
+          trainer.bag_entries.each { |bag_entry| bag_entry.each { |key, value| bag.add_item(key, value)} }
+          battle_info.add_party(bank, party, name, klass, battler, bag, nil, trainer.ai)
           # We add the base money only for the enemy side (prevents the ally to have a base money)
           battle_info.base_moneys[bank] << trainer.base_money if bank == 1
           battle_info.trainer_is_couple = battle_info.parties[1].size == 1 if bank == 1 && trainer.vs_type == 2
