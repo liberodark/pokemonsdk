a472e215de6531fd11691df0e7fd5b4bc8f186fb
Metaiko <65285292+Metaiko@users.noreply.github.com>
2022-08-06 17:04:13 +0200

    fix SFEMovie.so loading from Studio psdk-binaries

--- scripts/01450 Systems/10000 Movie/00099 Movie.rb
+++ scripts/01450 Systems/10000 Movie/00099 Movie.rb
@@ -74,7 +74,7 @@ module GamePlay
     end
 
     def auto_require_movie_player
-      filename = PSDK_RUNNING_UNDER_WINDOWS ? "#{ENV['GAMEDEPS'] || '.'}/lib/SFEMovie" : 'SFEMovie'
+      filename = PSDK_RUNNING_UNDER_WINDOWS ? "#{ENV['GAMEDEPS'] || ENV['PSDK_BINARY_PATH'] || '.'}/lib/SFEMovie" : 'SFEMovie'
       filename += PSDK_RUNNING_UNDER_MAC ? '.bundle' : '.so'
       require filename
     end
