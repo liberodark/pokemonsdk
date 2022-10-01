ee76f14aaaddb5bd08cf5ef9dcf3df4ab8e74a99
Nuri Yuri <qsdf@mlkjh.com>
2022-10-01 09:44:05 +0200

    Fix studio project not being able to migrate

--- scripts/tools/Studio/Main.rb
+++ scripts/tools/Studio/Main.rb
@@ -12,7 +12,7 @@ module Studio
 
     ScriptLoader.start
     ScriptLoader.load_tool('Studio/Handler')
-    PSDK_CONFIG.send(:initialize)
+    PSDK_CONFIG.send(:initialize) rescue nil # Normally not useful for post studio projects
     @handler = Handler.new
     @handler.start
   end
