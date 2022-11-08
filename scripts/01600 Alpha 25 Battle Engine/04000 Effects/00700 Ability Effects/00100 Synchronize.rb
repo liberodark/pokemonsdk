53d33524d6111f5ed8bc52155448cff945c393a4
Rey <3939415-TheRey@users.noreply.gitlab.com>
2022-11-08 15:37:47 +0000

    Correct a very small call order (doesn't need a MR for something so small)

--- scripts/01600 Alpha 25 Battle Engine/04000 Effects/00700 Ability Effects/00100 Synchronize.rb
+++ scripts/01600 Alpha 25 Battle Engine/04000 Effects/00700 Ability Effects/00100 Synchronize.rb
@@ -14,9 +14,9 @@ module Battle
           return if target != @target || launcher == target || !launcher || launcher.status == target.status
           return unless SYNCHRONIZED_STATUS.include?(status)
 
-          handler.status_change_with_process(status, launcher, target)
           handler.scene.visual.show_ability(target)
           handler.scene.display_message_and_wait(parse_text_with_pokemon(19, 1159, launcher))
+          handler.status_change_with_process(status, launcher, target)
         end
       end
       register(:synchronize, Synchronize)
