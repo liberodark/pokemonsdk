2b30815c4e11c7a01a4c6224ee930ea4e6d4d067
Aerun <2930148-Aerun@users.noreply.gitlab.com>
2023-01-10 10:14:11 +0000

    Fix Rototiller by removing old battle_stage_mod code

--- scripts/01600 Alpha 25 Battle Engine/04150 Battle_Move/00010 Definitions/00300 Rototiller.rb
+++ scripts/01600 Alpha 25 Battle Engine/04150 Battle_Move/00010 Definitions/00300 Rototiller.rb
@@ -18,19 +18,13 @@ module Battle
         return true
       end
 
-      # List the stats this move improves
-      # @return [Array<Integer>]
-      def battle_stage_mod
-        [1, 0, 0, 1, 0, 0, 0]
-      end
-
       private
 
       # Function that deals the stats to the pokemon
       # @param user [PFM::PokemonBattler] user of the move
       # @param actual_targets [Array<PFM::PokemonBattler>] targets that will be affected by the move
       def deal_stats(user, actual_targets)
-        super(user, logic.all_alive_battlers.select(&:type_grass?))
+        super(user, logic.all_alive_battlers.select { |target| target.type_grass? && target.grounded? })
       end
     end
     Move.register(:s_rototiller, Rototiller)
