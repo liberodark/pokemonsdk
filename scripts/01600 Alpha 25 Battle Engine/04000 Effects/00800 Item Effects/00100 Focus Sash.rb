3e2819a42a34f443ef892617ecfcc5084da3e4c8
Lexio <alexis.barreyre@gmail.com>
2023-02-13 13:47:11 +0000

    Fix Focus Sash display

--- scripts/01600 Alpha 25 Battle Engine/04000 Effects/00800 Item Effects/00100 Focus Sash.rb
+++ scripts/01600 Alpha 25 Battle Engine/04000 Effects/00800 Item Effects/00100 Focus Sash.rb
@@ -13,7 +13,22 @@ module Battle
           return unless skill && target == @target
           return if hp < target.hp || target.hp != target.max_hp
 
+          @show_message = true
           return target.hp - 1
+        end
+
+        # Function called after damages were applied (post_damage, when target is still alive)
+        # @param handler [Battle::Logic::DamageHandler]
+        # @param hp [Integer] number of hp (damage) dealt
+        # @param target [PFM::PokemonBattler]
+        # @param launcher [PFM::PokemonBattler, nil] Potential launcher of a move
+        # @param skill [Battle::Move, nil] Potential move used
+        def on_post_damage(handler, hp, target, launcher, skill)
+          return unless @show_message
+
+          @show_message = false
+          handler.scene.visual.show_item(target)
+          handler.scene.display_message_and_wait(parse_text_with_pokemon(19, 514, target))
         end
       end
       register(:focus_sash, FocusSash)
