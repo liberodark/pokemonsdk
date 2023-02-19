6f705b16ee66c07b782aa916cace349a6ae2546e
Lexio <alexis.barreyre@gmail.com>
2023-02-19 13:52:58 +0000

    Fix Taunt by adding move_disable_check

--- scripts/01600 Alpha 25 Battle Engine/04000 Effects/00500 Move Effects/01000 Taunt.rb
+++ scripts/01600 Alpha 25 Battle Engine/04000 Effects/00500 Move Effects/01000 Taunt.rb
@@ -16,6 +16,19 @@ module Battle
         @logic.scene.display_message_and_wait(message)
       end
 
+      # Function called when we try to check if the user cannot use a move
+      # @param user [PFM::PokemonBattler]
+      # @param move [Battle::Move]
+      # @return [Proc, nil]
+      def on_move_disabled_check(user, move)
+        return if user != @pokemon
+        return unless move.status?
+
+        return proc {
+          move.scene.display_message_and_wait(parse_text_with_pokemon(19, 571, user, PFM::Text::MOVE[1] => move.name)) 
+        }
+      end
+
       # Function called when we try to use a move as the user (returns :prevent if user fails)
       # @param user [PFM::PokemonBattler]
       # @param targets [Array<PFM::PokemonBattler>]
