1b2b8f31eed66a68ae394b3cd475e0dcddec2c70
Zøzo <benbar2412@gmail.com>
2023-02-19 13:25:23 +0000

    Fix Cursed Body

--- scripts/01600 Alpha 25 Battle Engine/04000 Effects/00700 Ability Effects/00100 Cursed Body.rb
+++ scripts/01600 Alpha 25 Battle Engine/04000 Effects/00700 Ability Effects/00100 Cursed Body.rb
@@ -10,8 +10,8 @@ module Battle
         # @param skill [Battle::Move, nil] Potential move used
         def on_post_damage(handler, hp, target, launcher, skill)
           return if target != @target || launcher == target
-          return unless skill&.direct? && launcher && launcher.hp > 0
-          return if (target.effects.has?(:substitute) && !skill.authentic?) || skill.db_symbol == :struggle
+          return unless skill&.direct? && launcher && launcher.hp > 0 && bchance?(0.3, @logic) && !launcher.has_ability?(:long_reach)
+          return if (target.effects.has?(:substitute) && !skill.authentic?) || skill.db_symbol == :struggle || target.effects.has?(:disable)
 
           handler.scene.visual.show_ability(target)
           launcher.effects.add(Effects::Disable.new(@logic, launcher, skill))
