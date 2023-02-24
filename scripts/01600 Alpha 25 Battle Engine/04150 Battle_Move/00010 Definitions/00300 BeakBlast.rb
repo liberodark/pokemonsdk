a61c5796a7e0426f9b2fc11511f60b1a20aac72d
Nathan Viguier <matatixofficiel77@gmail.com>
2023-02-24 18:42:08 +0000

    Add Beak Blast

---
+++ scripts/01600 Alpha 25 Battle Engine/04150 Battle_Move/00010 Definitions/00300 BeakBlast.rb
@@ -0,0 +1,22 @@
+module Battle
+  class Move
+    # Implement the Beak Blast move
+    class BeakBlast < Basic
+      # Is the move doing something before any other attack ?
+      # @return [Boolean]
+      def pre_attack?
+        true
+      end
+
+      # Proceed the procedure before any other attack.
+      # @param user [PFM::PokemonBattler]
+      def proceed_pre_attack(user)
+        @scene.display_message_and_wait(parse_text_with_pokemon(19, 616, user))
+        user.effects.add(Effects::BeakBlast.new(@logic, user))
+        # @todo play charging animation
+      end
+
+    end
+    Move.register(:s_beak_blast, BeakBlast)
+  end
+end
