8fd6b4304b46b22b93c603262f5a9a015987f140
Lexio <alexis.barreyre@gmail.com>
2023-02-24 21:23:27 +0000

    Fix Competitive and Defiant

--- scripts/01600 Alpha 25 Battle Engine/04000 Effects/00700 Ability Effects/00100 Defiant.rb
+++ scripts/01600 Alpha 25 Battle Engine/04000 Effects/00700 Ability Effects/00100 Defiant.rb
@@ -11,14 +11,30 @@ module Battle
         # @param skill [Battle::Move, nil] Potential move used
         # @return [Integer, nil] if integer, it will change the power
         def on_stat_change_post(handler, stat, power, target, launcher, skill)
-          return if target != @target
+          return if target != @target || target == launcher
+          return if @logic.allies_of(target).include?(launcher)
           return if power >= 0
 
           handler.scene.visual.show_ability(target)
-          handler.logic.stat_change_handler.stat_change_with_process(:atk, 2, target)
+          handler.logic.stat_change_handler.stat_change_with_process(stat_changed, 2, target)
+        end
+
+        # Stat changed by the ability
+        # @return [symbol] of the stat
+        def stat_changed
+          return :atk
+        end
+      end
+
+      class Competitive < Defiant
+        # Stat changed by the ability
+        # @return [symbol] of the stat
+        def stat_changed
+          return :ats
         end
       end
       register(:defiant, Defiant)
+      register(:competitive, Competitive)
     end
   end
 end
