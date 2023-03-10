8f2ec783864983e4cbeff06f06b7f6baa42ee5a6
Aerun <2930148-Aerun@users.noreply.gitlab.com>
2023-03-10 21:35:23 +0000

    Fix the tradeWith evolution

--- scripts/01450 Systems/00000 General/00001 PFM/00300 Pokemon/00400 Pokemon exp & evolve.rb
+++ scripts/01450 Systems/00000 General/00001 PFM/00300 Pokemon/00400 Pokemon exp & evolve.rb
@@ -186,7 +186,7 @@ module PFM
       return data_creature(expected_evolution.db_symbol).id, expected_evolution.form
     end
     # Exchanged with another pokemon
-    add_evolution_criteria(:tradeWith) { |value, extend_data| extend_data&.db_symbol == value }
+    add_evolution_criteria(:tradeWith, [:tradeWith]) { |value, extend_data, reason| extend_data&.db_symbol == value && reason == :tradeWith }
     # Minimum level
     add_evolution_criteria(:minLevel) { |value| @level >= value.to_i }
     # Maximum level
