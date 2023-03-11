6af580faadeed190d02320ddf0318f3b533fff1d
Zøzo <benbar2412@gmail.com>
2023-03-11 11:53:38 +0000

    Fix Conversion-2

--- scripts/01600 Alpha 25 Battle Engine/04150 Battle_Move/00010 Definitions/00300 Conversion.rb
+++ scripts/01600 Alpha 25 Battle Engine/04150 Battle_Move/00010 Definitions/00300 Conversion.rb
@@ -17,6 +17,10 @@ module Battle
 
     # Move that sets the type of the Pokemon as type of the last move used by target
     class Conversion2 < BasicWithSuccessfulEffect
+      # Return the exceptions to the Conversion 2 effect
+      # @return Array<Symbol>
+      MOVE_EXCEPTIONS = %i[revelation_dance struggle]
+
       # Function that tests if the user is able to use the move
       # @param user [PFM::PokemonBattler] user of the move
       # @param targets [Array<PFM::PokemonBattler>] expected targets
@@ -25,7 +29,7 @@ module Battle
       def move_usable_by_user(user, targets)
         return false unless super
 
-        if targets.none? { |target| target.move_history.any? }
+        if targets.none? { |target| target.move_history.any? && !MOVE_EXCEPTIONS.include?(target.move_history.last.db_symbol) && target.move_history.last.move.type != 0 }
           show_usage_failure(user)
           return false
         end
@@ -39,14 +43,21 @@ module Battle
       # @param user [PFM::PokemonBattler] user of the move
       # @param actual_targets [Array<PFM::PokemonBattler>] targets that will be affected by the move
       def deal_effect(user, actual_targets)
-        # @type [PFM::PokemonBattler]
-        last_move_user = actual_targets.find { |target| target.move_history.any? }
-        user.type1 = last_move_user&.move_history&.last&.move&.type || 0
+        last_move_user = actual_targets.max_by { |target| target.move_history.any? ? target.move_history.max_by(&:turn) : 0 }
+        type = last_move_user.move_history&.last&.move&.type || 0
+        user.type1 = random_resistances(type)
         user.type2 = 0
         @scene.display_message_and_wait(parse_text_with_pokemon(19, 899, user, '[VAR TYPE(0001)]' => data_type(user.type1).name))
       end
-    end
 
+      # Check the resistances to one type and return one random
+      # @param type [Integer] type of the move used by the target
+      # @return Integer
+      def random_resistances(move_type)
+        resistances = each_data_type.select { |type| data_type(move_type).hit(type.db_symbol) < 1 }
+        return resistances.sample.id
+      end
+    end
     Move.register(:s_conversion, Conversion)
     Move.register(:s_conversion2, Conversion2)
   end
