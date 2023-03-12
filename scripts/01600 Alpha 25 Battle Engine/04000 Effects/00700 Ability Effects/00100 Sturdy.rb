ca69ea6dc7a6f7bca9b376523e8082bf920335c8
Lexio <11392620-Lexiooo@users.noreply.gitlab.com>
2023-03-12 12:40:31 +0000

    Fix Sturdy

--- scripts/01600 Alpha 25 Battle Engine/04000 Effects/00700 Ability Effects/00100 Sturdy.rb
+++ scripts/01600 Alpha 25 Battle Engine/04000 Effects/00700 Ability Effects/00100 Sturdy.rb
@@ -13,8 +13,22 @@ module Battle
           return if target != @target || hp < target.hp || target.hp != target.max_hp || target == launcher
           return unless launcher&.can_be_lowered_or_canceled?
 
-          handler.scene.visual.show_ability(target)
+          @show_message = true
           return target.hp - 1
+        end
+
+        # Function called after damages were applied (post_damage, when target is still alive)
+        # @param handler [Battle::Logic::DamageHandler]
+        # @param hp [Integer] number of hp (damage) dealt
+        # @param target [PFM::PokemonBattler]
+        # @param launcher [PFM::PokemonBattler, nil] Potential launcher of a move
+        # @param skill [Battle::Move, nil] Potential move used
+        def on_post_damage(handler, hp, target, launcher, skill)
+          return unless @show_message
+
+          @show_message = false
+          handler.scene.visual.show_ability(target)
+          handler.scene.display_message_and_wait(parse_text_with_pokemon(19, 514, target))
         end
       end
       register(:sturdy, Sturdy)
