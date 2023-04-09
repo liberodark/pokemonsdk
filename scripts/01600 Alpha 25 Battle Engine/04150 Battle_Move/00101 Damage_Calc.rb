484851b4e0db8dc18afc94752d6e88ea3c6be23c
Zøzo <benbar2412@gmail.com>
2023-04-09 17:28:53 +0000

    Fix Substitute HP not taken into account when calculating damages

--- scripts/01600 Alpha 25 Battle Engine/04150 Battle_Move/00101 Damage_Calc.rb
+++ scripts/01600 Alpha 25 Battle Engine/04150 Battle_Move/00101 Damage_Calc.rb
@@ -18,23 +18,25 @@ module Battle
       log_data("# damages(#{user}, #{target}) for #{db_symbol}")
       # Reset the effectiveness
       @effectiveness = 1
-      @critical = logic.calc_critical_hit(user, target, critical_rate)        ; log_data("@critical = #{@critical} # critical_rate = #{critical_rate}")
-      damage = user.level * 2 / 5 + 2                                         ; log_data("damage = #{damage} # #{user.level} * 2 / 5 + 2")
-      damage = (damage * calc_base_power(user, target)).floor                 ; log_data("damage = #{damage} # after calc_base_power")
-      damage = (damage * calc_sp_atk(user, target)).floor / 50                ; log_data("damage = #{damage} # after calc_sp_atk / 50")
-      damage = (damage / calc_sp_def(user, target)).floor                     ; log_data("damage = #{damage} # after calc_sp_def")
-      damage = (damage * calc_mod1(user, target)).floor + 2                   ; log_data("damage = #{damage} # after calc_mod1 + 2")
-      damage = (damage * calc_ch(user, target)).floor                         ; log_data("damage = #{damage} # after calc_ch")
-      damage = (damage * calc_mod2(user, target)).floor                       ; log_data("damage = #{damage} # after calc_mod2")
+      @critical = logic.calc_critical_hit(user, target, critical_rate)                   ; log_data("@critical = #{@critical} # critical_rate = #{critical_rate}")
+      damage = user.level * 2 / 5 + 2                                                    ; log_data("damage = #{damage} # #{user.level} * 2 / 5 + 2")
+      damage = (damage * calc_base_power(user, target)).floor                            ; log_data("damage = #{damage} # after calc_base_power")
+      damage = (damage * calc_sp_atk(user, target)).floor / 50                           ; log_data("damage = #{damage} # after calc_sp_atk / 50")
+      damage = (damage / calc_sp_def(user, target)).floor                                ; log_data("damage = #{damage} # after calc_sp_def")
+      damage = (damage * calc_mod1(user, target)).floor + 2                              ; log_data("damage = #{damage} # after calc_mod1 + 2")
+      damage = (damage * calc_ch(user, target)).floor                                    ; log_data("damage = #{damage} # after calc_ch")
+      damage = (damage * calc_mod2(user, target)).floor                                  ; log_data("damage = #{damage} # after calc_mod2")
       damage *= logic.move_damage_rng.rand(calc_r_range)
-      damage /= 100                                                           ; log_data("damage = #{damage} # after rng")
+      damage /= 100                                                                      ; log_data("damage = #{damage} # after rng")
       types = definitive_types(user, target)
-      damage = (damage * calc_stab(user, types)).floor                        ; log_data("damage = #{damage} # after stab")
-      damage = (damage * calc_type_n_multiplier(target, :type1, types)).floor ; log_data("damage = #{damage} # after type1")
-      damage = (damage * calc_type_n_multiplier(target, :type2, types)).floor ; log_data("damage = #{damage} # after type2")
-      damage = (damage * calc_type_n_multiplier(target, :type3, types)).floor ; log_data("damage = #{damage} # after type3")
-      damage = (damage * calc_mod3(user, target)).floor                       ; log_data("damage = #{damage} # after mod3")
-      damage = damage.clamp(1, target.hp)                                     ; log_data("damage = #{damage} # after clamp")
+      damage = (damage * calc_stab(user, types)).floor                                   ; log_data("damage = #{damage} # after stab")
+      damage = (damage * calc_type_n_multiplier(target, :type1, types)).floor            ; log_data("damage = #{damage} # after type1")
+      damage = (damage * calc_type_n_multiplier(target, :type2, types)).floor            ; log_data("damage = #{damage} # after type2")
+      damage = (damage * calc_type_n_multiplier(target, :type3, types)).floor            ; log_data("damage = #{damage} # after type3")
+      damage = (damage * calc_mod3(user, target)).floor                                  ; log_data("damage = #{damage} # after mod3")
+      target_hp = target.effects.has?(:substitute) ? target.effects.get(:substitute).hp : target.hp
+      damage = damage.clamp(1, target_hp)                                                ; log_data("damage = #{damage} # after clamp")
+
       return damage
       # rubocop:enable Layout/ExtraSpacing
       # rubocop:enable Style/Semicolon
