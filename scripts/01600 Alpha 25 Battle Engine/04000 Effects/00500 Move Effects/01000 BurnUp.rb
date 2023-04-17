8f21548778c02a8b9e73915eb3caff1abf9e02ab
Zøzo <benbar2412@gmail.com>
2023-04-17 18:33:59 +0000

    Fix BurnUpMethod to create BurnUp-Like (lose plant type, lose electrik type etc...)

--- scripts/01600 Alpha 25 Battle Engine/04000 Effects/00500 Move Effects/01000 BurnUp.rb
+++ scripts/01600 Alpha 25 Battle Engine/04000 Effects/00500 Move Effects/01000 BurnUp.rb
@@ -7,9 +7,11 @@ module Battle
       # Create a new Pokemon tied effect
       # @param logic [Battle::Logic]
       # @param pokemon [PFM::PokemonBattler]
+      # @param type [Integer] the current type of the move
       # @param turn_count [Integer]
-      def initialize(logic, pokemon, turn_count)
+      def initialize(logic, pokemon, turn_count, type)
         super(logic, pokemon)
+        @type = type
         neutralize_type_initialize(pokemon, turn_count)
       end
 
@@ -24,7 +26,7 @@ module Battle
       # Get the neutralized types
       # @return [Array<Integer>]
       def neutralyzed_types
-        return [data_type(:fire).id]
+        return [data_type(@type).id]
       end
     end
   end
--- scripts/01600 Alpha 25 Battle Engine/04150 Battle_Move/00010 Definitions/00300 BurnUp.rb
+++ scripts/01600 Alpha 25 Battle Engine/04150 Battle_Move/00010 Definitions/00300 BurnUp.rb
@@ -2,7 +2,10 @@ module Battle
   class Move
     # Implement the Burn Up move
     class BurnUp < Basic
-
+      # Text of the loss of our type after launching the attack
+      TEXTS_IDS = {
+        burn_up: [:parse_text_with_pokemon, 59, 1856]
+      }
       # Function that tests if the user is able to use the move
       # @param user [PFM::PokemonBattler] user of the move
       # @param targets [Array<PFM::PokemonBattler>] expected targets
@@ -10,7 +13,7 @@ module Battle
       # @return [Boolean] if the procedure can continue
       def move_usable_by_user(user, targets)
         return false unless super
-        return show_usage_failure(user) && false unless user.type_fire?
+        return show_usage_failure(user) && false unless user.type?(type)
 
         return true
       end
@@ -19,8 +22,8 @@ module Battle
       # @param user [PFM::PokemonBattler] user of the move
       # @param actual_targets [Array<PFM::PokemonBattler>] targets that will be affected by the move
       def deal_effect(user, actual_targets)
-        user.effects.add(Effects::BurnUp.new(@logic, user, turn_count))
-        scene.display_message_and_wait(parse_text_with_pokemon(59, 1856, user))
+        user.effects.add(Effects::BurnUp.new(@logic, user, turn_count, type))
+        scene.display_message_and_wait(send(*TEXTS_IDS[db_symbol], user)) if TEXTS_IDS[db_symbol]
       end
 
       # Return the number of turns the effect works
