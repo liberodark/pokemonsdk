229ab79244e87fe3e5c685d6594b2f39da9ceafb
Lexio <11392620-Lexiooo@users.noreply.gitlab.com>
2023-05-07 12:33:07 +0000

    Fix walls and Fur Coat with Psyshock

--- scripts/01600 Alpha 25 Battle Engine/04000 Effects/00700 Ability Effects/00100 Fur Coat.rb
+++ scripts/01600 Alpha 25 Battle Engine/04000 Effects/00700 Ability Effects/00100 Fur Coat.rb
@@ -11,6 +11,7 @@ module Battle
           return 1 if target != @target
           return 1 unless user.can_be_lowered_or_canceled?
 
+          return 2 if move.be_method == :s_psyshock
           return move.physical? ? 2 : 1
         end
       end
