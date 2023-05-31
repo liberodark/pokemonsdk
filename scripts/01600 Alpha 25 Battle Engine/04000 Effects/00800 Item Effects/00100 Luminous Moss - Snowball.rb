6f416ad4c5a844b8fcb6c31c72eddc362d7a54e0
Zøzo <benbar2412@gmail.com>
2023-05-31 19:55:47 +0000

    Add AbsorbBulb && CellBattery + Add a proc condition

--- scripts/01600 Alpha 25 Battle Engine/04000 Effects/00800 Item Effects/00100 Luminous Moss - Snowball.rb
+++ scripts/01600 Alpha 25 Battle Engine/04000 Effects/00800 Item Effects/00100 Luminous Moss - Snowball.rb
@@ -10,7 +10,7 @@ module Battle
         # @param skill [Battle::Move, nil] Potential move used
         def on_post_damage(handler, hp, target, launcher, skill)
           return if target != @target
-          return unless expected_type?(skill)
+          return unless expected_type?(skill) && stat_changeable?(handler, target)
 
           handler.scene.visual.show_item(target)
           handler.logic.stat_change_handler.stat_change_with_process(stat_improved, 1, target)
@@ -31,6 +31,15 @@ module Battle
         def expected_type?(move)
           return move&.type_water?
         end
+
+        # Check if the change of stats is possible
+        # @param handler [Battle::Logic::DamageHandler]
+        # @param target [PFM::PokemonBattler]
+        # @return [Boolean]
+        def stat_changeable?(handler, target)
+          stat_handler = handler.logic.stat_change_handler
+          return target.has_ability?(:contrary) ? stat_handler.stat_decreasable?(stat_improved, target) : stat_handler.stat_increasable?(stat_improved, target)
+        end
       end
 
       class Snowball < LuminousMoss
@@ -49,8 +58,38 @@ module Battle
           return move&.type_ice?
         end
       end
+
+      class AbsorbBulb < LuminousMoss
+        private
+
+        # Get the stat the item should improve
+        # @return [Symbol]
+        def stat_improved
+          return :ats
+        end
+      end
+
+      class CellBattery < LuminousMoss
+        private
+
+        # Get the stat the item should improve
+        # @return [Symbol]
+        def stat_improved
+          return :atk
+        end
+
+        # Tell if the used skill triggers the effect
+        # @param move [Battle::Move, nil] Potential move used
+        # @return [Boolean]
+        def expected_type?(move)
+          return move&.type_electric?
+        end
+      end
+
       register(:luminous_moss, LuminousMoss)
       register(:snowball, Snowball)
+      register(:absorb_bulb, AbsorbBulb)
+      register(:cell_battery, CellBattery)
     end
   end
 end
