eca12e08fd34ef8ca28659e4ca068012d7bbdbe6
Zøzo <benbar2412@gmail.com>
2023-07-16 09:27:39 +0000

    Implement Eerie Spell

--- scripts/01600 Alpha 25 Battle Engine/04150 Battle_Move/00010 Definitions/00300 EerieSpell.rb
+++ scripts/01600 Alpha 25 Battle Engine/04150 Battle_Move/00010 Definitions/00300 EerieSpell.rb
@@ -1,23 +1,12 @@
 module Battle
   class Move
-    # Spite decreases the move's PP by exactly 4.
-    # @see https://bulbapedia.bulbagarden.net/wiki/Spite_(move)
-    # @see https://www.pokepedia.fr/Dépit
-    class Spite < Move
-      # Function that tests if the user is able to use the move
+    class EerieSpell < Basic
+      # Test if the effect is working
       # @param user [PFM::PokemonBattler] user of the move
-      # @param targets [Array<PFM::PokemonBattler>] expected targets
-      # @note Thing that prevents the move from being used should be defined by :move_prevention_user Hook
-      # @return [Boolean] if the procedure can continue
-      def move_usable_by_user(user, targets)
-        return false unless super
-
-        if targets.all? { |target| target.skills_set[find_last_skill_position(target)]&.pp == 0 || target.move_history.empty? }
-          show_usage_failure(user)
-          return false
-        end
-
-        return true
+      # @param actual_targets [Array<PFM::PokemonBattler>] targets that will be affected by the move
+      # @return [Boolean]
+      def effect_working?(user, actual_targets)
+        return actual_targets.all? { |target| target.move_history.any? && target.skills_set[find_last_skill_position(target)]&.pp != 0 }
       end
 
       private
@@ -28,26 +17,25 @@ module Battle
       def deal_effect(user, actual_targets)
         actual_targets.each do |target|
           last_skill = find_last_skill_position(target)
-          next unless target.skills_set[last_skill].pp > 0
+          next if target.move_history.empty? || target.skills_set[last_skill].pp == 0
 
-          num = 4.clamp(1, target.skills_set[last_skill].pp)
+          num = 3.clamp(1, target.skills_set[last_skill].pp)
           target.skills_set[last_skill].pp -= num
           scene.display_message_and_wait(parse_text_with_pokemon(19, 641, target, PFM::Text::MOVE[1] => target.skills_set[last_skill].name, '[VAR NUM1(0002)]' => num.to_s))
         end
       end
 
       # Find the last skill used position in the moveset of the Pokemon
-      # pokemon [PFM::PokemonBattler]
+      # @param pokemon [PFM::PokemonBattler]
       # @return [Integer]
       def find_last_skill_position(pokemon)
-        return 0 if pokemon.move_history.empty?
-
         pokemon.skills_set.each_with_index do |skill, i|
           return i if skill && skill.id == pokemon.move_history.last.move.id
         end
+
         return 0
       end
     end
-    Move.register(:s_spite, Spite)
+    Move.register(:s_eerie_spell, EerieSpell)
   end
 end
