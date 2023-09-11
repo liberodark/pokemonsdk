f6a243512baa7dd0be892f7d016c16a9e32ce80f
Zøzo <benbar2412@gmail.com>
2023-09-11 19:48:20 +0000

    Fix Bestow

--- scripts/01600 Alpha 25 Battle Engine/04150 Battle_Move/00010 Definitions/00300 Bestow.rb
+++ scripts/01600 Alpha 25 Battle Engine/04150 Battle_Move/00010 Definitions/00300 Bestow.rb
@@ -10,7 +10,8 @@ module Battle
       # @return [Boolean] if the procedure can continue
       def move_usable_by_user(user, targets)
         return false unless super
-        return false unless logic.item_change_handler.can_give_item?(user, targets.first)
+
+        return show_usage_failure(user) && false unless logic.item_change_handler.can_give_item?(user, targets.first)
 
         return true
       end
@@ -21,19 +22,21 @@ module Battle
       def deal_effect(user, actual_targets)
         target = actual_targets.first
         item = user.battle_item_db_symbol
+        item_name = user.item_name
+
         logic.item_change_handler.change_item(item, true, target, user, self)
         logic.item_change_handler.change_item(:none, true, user, user, self)
         logic.terrain_effects.add(Battle::Effects::Bestow.new(logic, user, target, item))
-        logic.scene.display_message_and_wait(give_text(user, target, user.item_name))
+        logic.scene.display_message_and_wait(give_text(target, user, item_name))
       end
 
       # Get the text displayed when the user gives its item to the target
-      # @param user [PFM::PokemonBattler] user of the move
       # @param target [Array<PFM::PokemonBattler>]
+      # @param user [PFM::PokemonBattler] user of the move
       # @param item [String] the name of the item
       # @return [String] the text to display
-      def give_text(user, target, item)
-        return parse_text_with_2pokemon(19, 1117, user, target, ::PFM::Text::ITEM2[2] => item)
+      def give_text(target, user, item)
+        return parse_text_with_2pokemon(19, 1117, target, user, PFM::Text::ITEM2[2] => item)
       end
     end
     Move.register(:s_bestow, Bestow)
