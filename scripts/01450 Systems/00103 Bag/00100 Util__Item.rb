65ab2b5aeb0bbcaf410f9668ad582b4c123809eb
Nuri Yuri <nuriyuri@pokemonworkshop.com>
2023-11-15 17:13:41 +0100

    Fix items not refreshing bag the list CU-860rjf0pj

--- scripts/01450 Systems/00103 Bag/00100 Util__Item.rb
+++ scripts/01450 Systems/00103 Bag/00100 Util__Item.rb
@@ -21,14 +21,7 @@ module Util
       elsif extend_data.open_party
         return util_item_open_party_sequence(extend_data, result_process)
       end
-      return util_item_on_use_sequence(extend_data)
-=begin
-      if extend_data[:action_to_push] || extend_data[:ball_data]
-        @return_data = [item.id, extend_data, false]
-        @running = false
-      end
-      return extend_data
-=end
+      return util_item_on_use_sequence(extend_data, result_process)
     end
 
     # Part where the extend_data request to open the party
@@ -54,8 +47,9 @@ module Util
 
     # Part where the extend_data request to use the item
     # @param extend_data [PFM::ItemDescriptor::Wrapper]
+    # @param result_process [Proc, nil]
     # @return [PFM::ItemDescriptor::Wrapper, false]
-    def util_item_on_use_sequence(extend_data)
+    def util_item_on_use_sequence(extend_data, result_process)
       message = parse_text(22, 46, PFM::Text::TRNAME[0] => $trainer.name,
                                    PFM::Text::ITEM2[1] => extend_data.item.exact_name)
 
@@ -63,7 +57,11 @@ module Util
         if extend_data.on_use(self) != :unused
           $bag.remove_item(extend_data.item.id, 1) if extend_data.item.is_limited
           display_message(message) if $scene == self
-          return_to_scene(Scene_Map) if $game_temp.common_event_id > 0
+          if $game_temp.common_event_id > 0
+            return_to_scene(Scene_Map)
+          else
+            result_process&.call
+          end
           return extend_data
         end
         return false
@@ -72,6 +70,7 @@ module Util
       $bag.remove_item(extend_data.item.id, 1) if extend_data.item.is_limited
       display_message(message)
       extend_data.on_use(self)
+      result_process&.call
 
       return extend_data
     end
