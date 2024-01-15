2ab35f788ec243921ba795868d2306c3474c487a
Zøzo <benbar2412@gmail.com>
2024-01-15 21:45:21 +0000

    Refacto Battle Bond & added the new 9G version of the ability

--- scripts/01600 Alpha 25 Battle Engine/04150 Battle_Move/00001 Mechanics/00103 TwoHit MultiHit.rb
+++ scripts/01600 Alpha 25 Battle Engine/04150 Battle_Move/00001 Mechanics/00103 TwoHit MultiHit.rb
@@ -15,7 +15,7 @@ module Battle
         @hit_amount.times.count do |i|
           next false unless actual_targets.all?(&:alive?)
           next false if user.dead?
-          
+
           @nb_hit += 1
           play_animation(user, actual_targets) if i > 0
           actual_targets.each do |target|
@@ -33,43 +33,43 @@ module Battle
         end
         @scene.display_message_and_wait(parse_text(18, 33, PFM::Text::NUMB[1] => @nb_hit.to_s))
         return false if user.dead?
-        
+
         return true
       end
-      
+
       # Check if this the last hit of the move
       # Don't call this method before deal_damage method call
       # @return [Boolean]
       def last_hit?
         return true if @user.dead?
         return true unless @actual_targets.all?(&:alive?)
-        
+
         return @hit_amount == @nb_hit
       end
-      
+
       # Tells if the move hits multiple times
       # @return [Boolean]
       def multi_hit?
         return true
       end
-      
+
       private
-      
+
       # Get the number of hit the move can perform
       # @param user [PFM::PokemonBattler] user of the move
       # @param actual_targets [Array<PFM::PokemonBattler>] targets that will be affected by the move
       # @return [Integer]
       def hit_amount(user, actual_targets)
         return 5 if user.has_ability?(:skill_link)
-        
+
         return MULTI_HIT_CHANCES.sample(random: @logic.generic_rng)
       end
     end
-    
+
     # Class describing a move hitting twice
     class TwoHit < MultiHit
       private
-      
+
       # Get the number of hit the move can perform
       # @param user [PFM::PokemonBattler] user of the move
       # @param actual_targets [Array<PFM::PokemonBattler>] targets that will be affected by the move
@@ -78,11 +78,11 @@ module Battle
         return 2
       end
     end
-    
+
     # Class describing a move hitting thrice
     class ThreeHit < MultiHit
       private
-      
+
       # Get the number of hit the move can perform
       # @param user [PFM::PokemonBattler] user of the move
       # @param actual_targets [Array<PFM::PokemonBattler>] targets that will be affected by the move
@@ -91,7 +91,7 @@ module Battle
         return 3
       end
     end
-    
+
     # This method applies for triple kick and triple axel : power ramps up but the move stops if the subsequent attack misses.
     class TripleKick < MultiHit
       # Get the real base power of the move (taking in account all parameter)
@@ -102,9 +102,9 @@ module Battle
         final_power = power + @nb_hit * power
         return final_power
       end
-      
+
       private
-      
+
       # Function that deals the damage to the pokemon
       # @param user [PFM::PokemonBattler] user of the move
       # @param actual_targets [Array<PFM::PokemonBattler>] targets that will be affected by the move
@@ -117,7 +117,7 @@ module Battle
           next false unless actual_targets.all?(&:alive?)
           next false if user.dead?
           next false if i > 0 && !user.has_ability?(:skill_link) && (actual_targets = recalc_targets(user, actual_targets)).empty?
-          
+
           play_animation(user, actual_targets) if i > 0
           actual_targets.each do |target|
             hp = damages(user, target)
@@ -135,53 +135,60 @@ module Battle
         end
         @scene.display_message_and_wait(parse_text(18, 33, PFM::Text::NUMB[1] => @nb_hit.to_s))
         return false if user.dead?
-        
+
         return true
       end
-      
+
       # Recalculate the target each time it's needed
       # @param user [PFM::PokemonBattler] user of the move
       # @param targets [Array<PFM::PokemonBattler>] the current targets we need the accuracy recalculation on
       def recalc_targets(user, targets)
         # => proceed_move_accuracy will call display message if failure
         return [] unless proceed_move_accuracy(user, targets) || (on_move_failure(user, targets, :accuracy) && false)
-        
+
         user, targets = proceed_battlers_remap(user, targets)
-        
+
         actual_targets = accuracy_immunity_test(user, targets) # => Will call $scene.dislay_message for each accuracy fail
         return [] if actual_targets.none? && (on_move_failure(user, targets, :immunity) || true)
-        
+
         return actual_targets
         # rubocop:enable Lint/LiteralAsCondition
       end
-      
+
       def hit_amount(user, actual_targets)
         return 3
       end
     end
-    
+
     # Class describing Water Shuriken : Changes power and number of hit depending on greninja's base or Ash form.
     class WaterShuriken < MultiHit
+      # New version of the Greninja ability (9G+)
+      BATTLE_BOND_GEN_NINE = false
       # Get the real base power of the move (taking in account all parameter)
       # @param user [PFM::PokemonBattler] user of the move
       # @param target [PFM::PokemonBattler] target of the move
       # @return [Integer]
       def real_base_power(user, target)
-        modified_power = 20 if user.db_symbol == :greninja && user.form == 1
-        return modified_power || power
+        return super unless user.db_symbol == :greninja
+        return super if (user.form != 1 || BATTLE_BOND_GEN_NINE)
+
+        modified_power = 20
+        log_data("Water Shuriken Power = #{modified_power}")
+        return modified_power
       end
-      
+
       # Get the number of hit the move can perform
       # @param user [PFM::PokemonBattler] user of the move
       # @param actual_targets [Array<PFM::PokemonBattler>] targets that will be affected by the move
       # @return [Integer]
       def hit_amount(user, actual_targets)
-        return 3 if user.db_symbol == :greninja && user.form == 1
-        
-        return super
+        return super unless user.db_symbol == :greninja
+        return super if (user.form != 1 || BATTLE_BOND_GEN_NINE)
+
+        return 3
       end
     end
-    
+
     Move.register(:s_multi_hit, MultiHit)
     Move.register(:s_2hits, TwoHit)
     Move.register(:s_3hits, ThreeHit)
