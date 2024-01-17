a325fd199d3cb3840e9b52bcf3f0750ce0d0d7a3
NuriYuri <youri.54@live.fr>
2024-01-17 21:32:05 +0000

    Add generate_std_lib utility

--- scripts/tools/GameLoader/Z_load_uncompiled.rb
+++ scripts/tools/GameLoader/Z_load_uncompiled.rb
@@ -3,6 +3,9 @@ PARGV.parse
 ScriptLoader.load_tool('Studio/Main') if PARGV.game_launched_by_studio?
 ScriptLoader.load_tool('GameLoader/1_setupConstantAndLoadPath')
 ScriptLoader.load_tool('GameLoader/2_displayException')
+if File.exist?(File.join(PSDK_LIB_PATH, 'ruby-lib'))
+  ScriptLoader.load_tool('GameLoader/3_load_ruby_lib')
+end
 ScriptLoader.load_tool('GameLoader/3_load_extensions')
 ScriptLoader.load_tool('GameLoader/31_ruby_dependencies')
 ScriptLoader.load_tool('GameLoader/32_console_uncompiled')
