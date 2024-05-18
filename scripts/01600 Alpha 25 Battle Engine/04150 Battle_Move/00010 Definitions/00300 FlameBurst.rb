653362d9dc537ff6975d59e4a1eb074441f6aea5
Zøzo <benbar2412@gmail.com>
2024-05-18 14:52:16 +0200

    Fix Flame Burst

--- scripts/01600 Alpha 25 Battle Engine/04150 Battle_Move/00010 Definitions/00300 FlameBurst.rb
+++ scripts/01600 Alpha 25 Battle Engine/04150 Battle_Move/00010 Definitions/00300 FlameBurst.rb
@@ -1,37 +1,35 @@
 module Battle
   class Move
     # Flame Burst deals damage and will also cause splash damage to any Pokémon adjacent to the target.
-    # @see https://pokemondb.net/move/flame-burst
-    # @see https://bulbapedia.bulbagarden.net/wiki/Flame_Burst_(move)
-    # @see https://www.pokepedia.fr/Rebondifeu
     class FlameBurst < Basic
       # Function that deals the damage to the pokemon
       # @param user [PFM::PokemonBattler] user of the move
       # @param actual_targets [Array<PFM::PokemonBattler>] targets that will be affected by the move
       def deal_damage(user, actual_targets)
         super
-        splash_targets = []
-        splash_damages = []
-        actual_targets.each do |target|
-          targets = logic.adjacent_allies_of(target)
-          targets.each do |sub_target|
-            damage = calc_splash_damage(user, target)
-            logic.damage_handler.damage_change(damage, sub_target)
-            splash_targets << sub_target
-            splash_damages << -damage
+
+        actual_targets.each do |actual_target|
+          targets = logic.adjacent_allies_of(actual_target)
+
+          targets.each do |target|
+            next if target.has_ability?(:magic_guard)
+
+            hp = calc_splash_damage(target)
+            logic.damage_handler.damage_change(hp, target)
           end
         end
-        scene.visual.show_hp_animations(splash_targets, splash_damages)
       end
 
+      private
+
       # Calculate the damage dealt by the splash
-      # @param user [PFM::PokemonBattler] user of the move
       # @param target [PFM::PokemonBattler] target of the splash
       # @return [Integer]
-      def calc_splash_damage(user, target)
-        return (target.max_hp / 16)
+      def calc_splash_damage(target)
+        return (target.max_hp / 16).clamp(1, target.hp)
       end
     end
+
     register(:s_flame_burst, FlameBurst)
   end
 end
