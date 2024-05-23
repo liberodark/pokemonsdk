638e1b7dd71158572ff293d8111e362f4e81e739
Zøzo <benbar2412@gmail.com>
2024-05-23 21:59:01 +0200

    Rework Ice Face

--- scripts/01600 Alpha 25 Battle Engine/04000 Effects/00700 Ability Effects/00100 Ice Face.rb
+++ scripts/01600 Alpha 25 Battle Engine/04000 Effects/00700 Ability Effects/00100 Ice Face.rb
@@ -2,6 +2,19 @@ module Battle
   module Effects
     class Ability
       class IceFace < Ability
+        # Function called when a Pokemon has actually switched with another one
+        # @param handler [Battle::Logic::SwitchHandler]
+        # @param who [PFM::PokemonBattler] Pokemon that is switched out
+        # @param with [PFM::PokemonBattler] Pokemon that is switched in
+        def on_switch_event(handler, who, with)
+          return if who == with
+          return if who != @target
+          return if who.form == 0
+          return unless $env.hail?
+
+          who.form_calibrate(:base)
+        end
+
         # Function called when a damage_prevention is checked
         # @param handler [Battle::Logic::DamageHandler]
         # @param hp [Integer] number of hp (damage) dealt
@@ -10,48 +23,31 @@ module Battle
         # @param skill [Battle::Move, nil] Potential move used
         # @return [:prevent, Integer, nil] :prevent if the damage cannot be applied, Integer if the hp variable should be updated
         def on_damage_prevention(handler, hp, target, launcher, skill)
-          return if target != @target || target.effects.has?(:heal_block) || !skill&.physical?
+          return if target != @target || launcher == @target
+          return if target.form != 0
+          return unless skill&.physical?
           return unless launcher&.can_be_lowered_or_canceled?
 
-          @original_form = @target.form
-          @target.form_calibrate(:battle)
-          @hail = $env.hail?
-
-          if @target.form != @original_form
-            return handler.prevent_change do
-              handler.scene.visual.show_ability(target)
-              handler.scene.visual.show_switch_form_animation(target)
-            end
-          end
-        end
-
-        # Function called when a weather_prevention is checked
-        # @param handler [Battle::Logic::WeatherChangeHandler]
-        # @param weather_type [Symbol] - :none, :rain, :sunny, :sandstorm, :hail, :fog
-        # @param last_weather [Symbol] - :none, :rain, :sunny, :sandstorm, :hail, :fog
-
-        def on_weather_prevention(handler, weather_type, last_weather)
-          return if target != @target
-
-          return unless weather_type == :hail && @hail == false
-
-          @original_form = @target.form
-          @target.form_calibrate
-          @hail = true
-          if @target.form != @original_form
-            handler.prevent_change
+          return handler.prevent_change do
             handler.scene.visual.show_ability(target)
+            handler.scene.visual.wait_for_animation
+            target.form_calibrate(:battle)
             handler.scene.visual.show_switch_form_animation(target)
-            super
           end
         end
 
-        # Function called at the end of a turn
-        # logic [Battle::Logic] - logic of the battle
-        # scene [Battle::Scene] - battle scene
-        # battlers [Array<PFM::PokemonBattler>] - all alive battlers
-        def on_end_turn_event(logic, scene, battlers)
-          @hail = $env.hail?
+        # Function called after the weather was changed (on_post_weather_change)
+        # @param handler [Battle::Logic::WeatherChangeHandler]
+        # @param weather_type [Symbol] :none, :rain, :sunny, :sandstorm, :hail, :fog
+        # @param last_weather [Symbol] :none, :rain, :sunny, :sandstorm, :hail, :fog
+        def on_post_weather_change(handler, weather_type, last_weather)
+          return unless weather_type == :hail
+          return if @target.form == 0
+
+          handler.scene.visual.show_ability(@target)
+          handler.scene.visual.wait_for_animation
+          @target.form_calibrate(:base)
+          handler.scene.visual.show_switch_form_animation(@target)
         end
 
         # Function called after damages were applied and when target died (post_damage_death)
@@ -62,10 +58,12 @@ module Battle
         # @param skill [Battle::Move, nil] Potential move used
         def on_post_damage_death(handler, hp, target, launcher, skill)
           return if target != @target
+          return if target.form == 0
 
-          target.form = 0
+          target.form_calibrate(:base)
         end
       end
+
       register(:ice_face, IceFace)
     end
   end
