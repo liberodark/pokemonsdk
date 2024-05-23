5fe00b47ad871d8a94595fedacddf5ab4a0f3569
Zøzo <benbar2412@gmail.com>
2024-05-23 21:52:02 +0000

    Behaviour added for Disguise

--- scripts/01600 Alpha 25 Battle Engine/04000 Effects/00700 Ability Effects/00100 Disguise.rb
+++ scripts/01600 Alpha 25 Battle Engine/04000 Effects/00700 Ability Effects/00100 Disguise.rb
@@ -10,18 +10,20 @@ module Battle
         # @param skill [Battle::Move, nil] Potential move used
         # @return [:prevent, Integer, nil] :prevent if the damage cannot be applied, Integer if the hp variable should be updated
         def on_damage_prevention(handler, hp, target, launcher, skill)
-          return if target != @target || target.effects.has?(:heal_block) || skill&.status?
+          return if target != @target || target.form != 0 || target.effects.has?(:substitute)
+          return if skill.nil? || skill.status?
           return unless launcher&.can_be_lowered_or_canceled?
 
-          original_form = target.form
-          target.form_calibrate(:battle)
+          hp = (target.max_hp / 8).clamp(1, target.hp)
 
-          if target.form != original_form
-            return handler.prevent_change do
-              handler.scene.visual.show_ability(target)
-              handler.scene.visual.show_switch_form_animation(target)
-              handler.scene.visual.show_hp_animations([target], [-target.max_hp / 8])
-            end
+          return handler.prevent_change do
+            handler.scene.visual.show_ability(target)
+            handler.logic.damage_handler.damage_change(hp, target)
+            handler.scene.display_message_and_wait(parse_text(60, 364))
+            target.form_calibrate(:battle)
+            handler.scene.visual.show_switch_form_animation(target)
+            handler.scene.display_message_and_wait(parse_text_with_pokemon(59, 1910, target))
+            reset_types(target)
           end
         end
 
@@ -32,11 +34,22 @@ module Battle
         # @param launcher [PFM::PokemonBattler, nil] Potential launcher of a move
         # @param skill [Battle::Move, nil] Potential move used
         def on_post_damage_death(handler, hp, target, launcher, skill)
-          return if target != @target
+          return if target != @target || target.form == 0
 
-          target.form = 0
+          target.form_calibrate(:base)
+        end
+
+        private
+
+        # Recovers its original types
+        # @param target [PFM::PokemonBattler]
+        def reset_types(target)
+          target.type1 = data_type(target.data.type1).id
+          target.type2 = data_type(target.data.type2).id
+          target.type3 = 0
         end
       end
+
       register(:disguise, Disguise)
     end
   end
