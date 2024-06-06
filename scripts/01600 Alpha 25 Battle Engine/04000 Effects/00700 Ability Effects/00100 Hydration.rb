6c399fb398a8fe4f60bf90b062735f3f3933a8fd
Zøzo <benbar2412@gmail.com>
2024-06-06 19:03:16 +0000

    Small adjustment to the behavior of the hydration ability in rainy weather

--- scripts/01600 Alpha 25 Battle Engine/04000 Effects/00700 Ability Effects/00100 Hydration.rb
+++ scripts/01600 Alpha 25 Battle Engine/04000 Effects/00700 Ability Effects/00100 Hydration.rb
@@ -7,13 +7,16 @@ module Battle
         # @param scene [Battle::Scene] battle scene
         # @param battlers [Array<PFM::PokemonBattler>] all alive battlers
         def on_end_turn_event(logic, scene, battlers)
-          return unless battlers.include?(@target) && $env.rain?
+          return unless battlers.include?(@target)
           return if @target.dead?
+          return unless $env.rain? || $env.hardrain?
+          return unless @target.status?
 
           scene.visual.show_ability(@target)
           logic.status_change_handler.status_change(:cure, @target)
         end
       end
+
       register(:hydration, Hydration)
     end
   end
