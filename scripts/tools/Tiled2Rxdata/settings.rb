dbb2774a8d3089ba032105d9cbfc4dee23a74c42
Nuri Yuri <yuri@pokemonworkshop.com>
2024-06-15 11:41:20 +0200

    Fix name= and duplicate tileset_id issue in Tiled2Rxdata

--- scripts/tools/Tiled2Rxdata/settings.rb
+++ scripts/tools/Tiled2Rxdata/settings.rb
@@ -23,11 +23,34 @@ module Tiled2Rxdata
       return id
     end
 
+    # Function responsive of making sure that tilesets and @map_to_tileset are somewhate coherent
+    def auto_fix_tilesets
+      # No fixup if the @map_to_tileset is empty
+      return if @map_to_tileset.empty?
+
+      tileset_ids = @map_to_tileset.values
+      max_tileset_id = tileset_ids.max
+      # Remap tileset ids to ensure there is no duplicate (set duplicate to max_tileset_id + 1)
+      tileset_ids = tileset_ids.map.with_index do |id, index|
+        if tileset_ids.index(id) != index
+          max_tileset_id += 1
+          next max_tileset_id
+        end
+        next id
+      end
+      # Rebuild the @map_to_tileset with new tileset_ids table
+      @map_to_tileset = @map_to_tileset.keys.zip(tileset_ids).to_h
+      # Fill the tilesets table until all ids are set (fixup the undefined method #name= issue)
+      TILESETS << TILESETS[1].dup while TILESETS.size <= max_tileset_id
+      # Ensure the load function gets
+      return self
+    end
+
     class << self
       # Load the settings
       # @return [Settings]
       def load
-        File.exist?(FILENAME) ? load_data(FILENAME) : new
+        (File.exist?(FILENAME) ? load_data(FILENAME) : new).auto_fix_tilesets
       end
 
       # Save the settings
