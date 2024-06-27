131d1b5c045e35fa0c43ace0d2d1646acf418592
Lexio <11392620-Lexiooo@users.noreply.gitlab.com>
2024-06-27 17:27:50 +0000

    Add Shed Tail move

--- scripts/01600 Alpha 25 Battle Engine/04150 Battle_Move/00010 Definitions/00300 Substitute.rb
+++ scripts/01600 Alpha 25 Battle Engine/04150 Battle_Move/00010 Definitions/00300 Substitute.rb
@@ -1,6 +1,6 @@
 module Battle
   class Move
-    # Move that put the mon into a substitue
+    # Move that put the mon into a substitute
     class Substitute < BasicWithSuccessfulEffect
       # Function that tests if the user is able to use the move
       # @param user [PFM::PokemonBattler] user of the move
@@ -67,6 +67,43 @@ module Battle
       end
     end
 
+    # Move that put the mon into a substitute and switches it out, giving its sub to the incoming Pokémon
+    class ShedTail < Substitute
+      # Tell if the move is a move that switch the user if that hit
+      def self_user_switch?
+        return true
+      end
+
+      # Function that tests if the user is able to use the move
+      # @param user [PFM::PokemonBattler] user of the move
+      # @param targets [Array<PFM::PokemonBattler>] expected targets
+      # @note Thing that prevents the move from being used should be defined by :move_prevention_user Hook
+      # @return [Boolean] if the procedure can continue
+      def move_usable_by_user(user, targets)
+        return false unless super
+        return show_usage_failure(user) && false unless @logic.switch_handler.can_switch?(user, self)
+
+        return true
+      end
+
+      # Function that deals the effect to the pokemon
+      # @param user [PFM::PokemonBattler] user of the move
+      # @param actual_targets [Array<PFM::PokemonBattler>] targets that will be affected by the move
+      def deal_effect(user, actual_targets)
+        super
+        actual_targets.each do |target|
+          target.effects.add(Battle::Effects::ShedTail.new(logic, target))
+          logic.request_switch(target, nil)
+        end
+      end
+
+      # The divisor used to calculate the HP cost for creating a substitute (1/4 of max HP)
+      # @return [Integer]
+      def factor
+        return 2
+      end
+    end
     Move.register(:s_substitute, Substitute)
+    Move.register(:s_shed_tail, ShedTail)
   end
 end
