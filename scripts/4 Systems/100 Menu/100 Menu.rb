ee63d55aea60f0f266c158a86077fa920091b43a
NuriYuri <youri.54@live.fr>
2024-08-24 11:22:39 +0000

    Improve menu

--- scripts/4 Systems/100 Menu/100 Menu.rb
+++ scripts/4 Systems/100 Menu/100 Menu.rb
@@ -1,11 +1,13 @@
 module GamePlay
   # Main menu UI
-  #
-  # Rewritten thanks to Jaizu demand
-  class Menu < BaseCleanUpdate::FrameBalanced
+  class Menu < BaseCleanUpdate
     include MenuMixin
-    # List of action according to the "image_index" to call
-    ACTION_LIST = %i[open_dex open_party open_bag open_tcard open_option open_save open_quit]
+    # List of action according to call based on button index
+    ACTION_LIST = []
+    # List of condition to verify in order to show/activate a button
+    CONDITION_LIST = []
+    # List of button overwrites
+    BUTTON_OVERWRITES = []
     # Entering - leaving animation offset
     ENTERING_ANIMATION_OFFSET = 150
     # Entering - leaving animation duration
@@ -21,9 +23,8 @@ module GamePlay
       @max_index = @image_indexes.size - 1
       @quiting = false # Flag allowing to really quit
       @entering = true # Flag telling we're entering
-      @counter = 0 # Animation counter
       @in_save = false
-      @mbf_type = @mef_type = :noen if $scene.is_a?(Scene_Map)
+      @mbf_type = @mef_type = :none if $scene.is_a?(Scene_Map)
     end
 
     # Create all the graphics
@@ -44,13 +45,14 @@ module GamePlay
     # @return [Boolean] if no input was detected
     def update_inputs
       return false if @entering || @quiting
+
       if index_changed(:@index, :UP, :DOWN, @max_index)
         play_cursor_se
         update_buttons
       elsif Input.trigger?(:A)
         action
       elsif Input.trigger?(:B)
-        @running = false
+        @quiting = true
       else
         return true
       end
@@ -83,12 +85,6 @@ module GamePlay
 
     # Update the graphics
     def update_graphics
-      # Little trick to allow quitting animation ;)
-      unless @running || @quiting
-        @quiting = true
-        @running = true
-        @__last_scene.spriteset.visible = true if @__last_scene.is_a?(Scene_Map)
-      end
       # Update each animation
       if @entering
         update_entering_animation
@@ -109,60 +105,108 @@ module GamePlay
       end
     end
 
+    def fade_out(*)
+      return unless @running
+
+      animation = create_quitting_animation
+      until animation.done?
+        animation.update
+        Graphics.update
+      end
+    end
+
+    def fade_in(*)
+      return if @entering
+      Graphics.transition(8)
+
+      @buttons[@index].selected = true
+      animation = create_entering_animation
+      until animation.done?
+        animation.update
+        Graphics.update
+      end
+    end
+
     private
 
     # Animation played during enter sequence
     def update_entering_animation
-      @buttons.each { |button| button.move(-ENTERING_ANIMATION_OFFSET / ENTERING_ANIMATION_DURATION, 0) }
-      @background.opacity += 255 / ENTERING_ANIMATION_DURATION
-      @counter += 1
-      if @counter >= ENTERING_ANIMATION_DURATION
-        @counter = 0
+      @animation = create_entering_animation unless @animation
+      @animation.update
+      if @animation.done?
         @entering = false
-        update_buttons
-        @background.opacity = 255
         @__last_scene.spriteset.visible = false if @__last_scene.is_a?(Scene_Map)
+        update_buttons
+        @animation = nil
       end
+    end
+
+    def create_entering_animation
+      ya = Yuki::Animation
+      animation = ya.opacity_change(0.25, @background, 0, 255)
+      @buttons.each do |button|
+        animation.parallel_play(ya.move_discreet(0.25, button, button.x, button.y, button.x - ENTERING_ANIMATION_OFFSET, button.y))
+      end
+      animation.start
+      return animation
     end
 
     # Animation played during the quit sequence
     def update_quitting_animation
-      @buttons.each { |button| button.move(ENTERING_ANIMATION_OFFSET / ENTERING_ANIMATION_DURATION, 0) }
-      @background.opacity -= 255 / ENTERING_ANIMATION_DURATION
-      @counter += 1
-      @running = false if @counter >= ENTERING_ANIMATION_DURATION
+      @animation = create_quitting_animation unless @animation
+      @animation.update
+      if @animation.done?
+        @running = false
+        @animation = nil
+      end
+    end
+
+    def create_quitting_animation
+      @__last_scene.spriteset.visible = true if @__last_scene.is_a?(Scene_Map) && !@animation
+      ya = Yuki::Animation
+      animation = ya.opacity_change(0.25, @background, 255, 0)
+      @buttons.each do |button|
+        button.selected = false
+        animation.parallel_play(ya.move_discreet(0.25, button, button.x, button.y, button.x + ENTERING_ANIMATION_OFFSET, button.y))
+      end
+      animation.start
+      return animation
     end
 
     # Create the conditional array telling which scene is enabled
     def init_conditions
-      @conditions =
-        [
-          $game_switches[Yuki::Sw::Pokedex], # Pokedex
-          $actors.any?, # Party
-          !$bag.locked, # Bag
-          true, # Trainer card
-          true, # Options
-          !$game_system.save_disabled, # Save
-          true
-        ]
+      # @type [Array<Boolean>]
+      @conditions = CONDITION_LIST.map(&:call)
     end
 
     # Init the image_indexes array
     def init_indexes
+      # @type [Array<Integer>]
       @image_indexes = @conditions.collect.with_index { |condition, index| condition ? index : nil }
       @image_indexes.compact!
+      push_quit_at_the_end
+    end
+
+    def push_quit_at_the_end
+      quit_index = ACTION_LIST.index(:open_quit)
+      return unless quit_index && @image_indexes.include?(quit_index)
+      
+      @image_indexes.delete(quit_index)
+      @image_indexes << quit_index
     end
 
     # Create the background image (blur)
     def create_background
-      add_disposable @background = UI::BlurScreenshot.new(@viewport, @__last_scene)
-      @background.opacity -= 255 / ENTERING_ANIMATION_DURATION * ENTERING_ANIMATION_DURATION
+      add_disposable(@background = UI::BlurScreenshot.new(@viewport, @__last_scene))
+      @background.opacity = 0
     end
 
     # Create the menu buttons
     def create_buttons
-      @buttons = Array.new(@image_indexes.size) do |i|
-        UI::PSDKMenuButton.new(@viewport, @image_indexes[i], i)
+      # @type [Array<UI::PSDKMenuButtonBase>]
+      @buttons = @image_indexes.map.with_index do |real_index, i|
+        klass = BUTTON_OVERWRITES[real_index]&.call || UI::PSDKMenuButtonBase
+        next klass.new(@viewport, real_index, i)
       end
     end
 
@@ -192,7 +236,7 @@ module GamePlay
       GamePlay.open_party_menu do |scene|
         Yuki::FollowMe.update
         @background.update_snapshot
-        if scene.call_skill_process
+        if scene.call_skill_process || $game_temp.common_event_id != 0
           @call_skill_process = scene.call_skill_process
           @running = false
           Graphics.transition
@@ -203,7 +247,6 @@ module GamePlay
     # Open the Bag UI
     def open_bag
       GamePlay.open_bag
-      Graphics.transition unless @running
     end
 
     # Open the TCard UI
@@ -233,8 +276,42 @@ module GamePlay
 
     # Quit the scene
     def open_quit
-      @running = false
+      @quiting = true
     end
+
+    class << self
+      # Register a button in the UI
+      # @param action [Symbol] method to call when the button is clicked
+      # @param condition [Proc] block called to verify the condition
+      def register_button(action, &condition)
+        ACTION_LIST << action
+        CONDITION_LIST << condition
+      end
+
+      # Register a button overwrite
+      # @param index [Integer] index of the button
+      # @param block [Proc, nil] proc called to get the overwrite button class to use
+      def register_button_overwrite(index, &block)
+        BUTTON_OVERWRITES[index] = block
+      end
+
+      # Clear all the thing that was previously registered so you can do it your way
+      def clear_previous_registers
+        ACTION_LIST.clear
+        CONDITION_LIST.clear
+        BUTTON_OVERWRITES.clear
+      end
+    end
+
+    register_button(:open_dex) { $game_switches[Yuki::Sw::Pokedex] }
+    register_button(:open_party) { $actors.any? }
+    register_button(:open_bag) { !$bag.locked }
+    register_button(:open_tcard) { true }
+    register_button(:open_option) { true }
+    register_button(:open_save) { !$game_system.save_disabled }
+    register_button(:open_quit) { true }
+
+    register_button_overwrite(2) { $trainer.playing_girl ? UI::GirlBagMenuButton : nil }
   end
 end
 
