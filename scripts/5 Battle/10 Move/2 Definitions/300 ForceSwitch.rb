d64af52004c863eb247b5ce1c289ceb0a02b5b65
joeyw <joeyw@free.fr>
2024-09-16 17:23:13 +0000

    Prevent Roar to be used while under Sleep

--- scripts/5 Battle/10 Move/2 Definitions/300 ForceSwitch.rb
+++ scripts/5 Battle/10 Move/2 Definitions/300 ForceSwitch.rb
@@ -17,7 +17,7 @@ module Battle
       # @note Thing that prevents the move from being used should be defined by :move_prevention_user Hook
       # @return [Boolean] if the procedure can continue
       def move_usable_by_user(user, targets)
-        return true unless super
+        return false unless super
         return true unless be_method == :s_roar
 
         if targets.all? { |target| target.effects.has?(:crafty_shield) || target.has_ability?(:guard_dog) }
