04462ae03059b6e032af426be0aee2a7eb2499a1
Zøzo <benbar2412@gmail.com>
2024-11-12 20:38:07 +0000

    Fix pre mega-evolution message for Rayquaza and add post mega-evolution message

--- scripts/5 Battle/05 Actions/003 Mega.rb
+++ scripts/5 Battle/05 Actions/003 Mega.rb
@@ -5,6 +5,7 @@ module Battle
       # Get the user of this action
       # @return [PFM::PokemonBattler]
       attr_reader :user
+
       # Create a new mega evolution action
       # @param scene [Battle::Scene]
       # @param user [PFM::PokemonBattler]
@@ -29,24 +30,36 @@ module Battle
       # Execute the action
       def execute
         @scene.logic.mega_evolve.mark_as_mega_evolved(@user)
-        @scene.display_message_and_wait(message)
+        @scene.display_message_and_wait(pre_mega_evolution_message)
         @user.mega_evolve
         @scene.visual.show_switch_form_animation(@user)
+        @scene.display_message_and_wait(post_mega_evolution_message)
         @user.ability_effect.on_switch_event(@scene.logic.switch_handler, @user, @user)
-        # TODO!
       end
 
       private
 
-      # Get the mega evolve message
+      # Get the pre mega evolve message
       # @return [String]
-      def message
+      def pre_mega_evolution_message
+        mega_evolution_with_mega_stone = @user.data.evolutions.find { |evolution| evolution.condition_data(:gemme) == @user.battle_item_db_symbol }
+        return parse_text_with_pokemon(19, 1247, @user, PFM::Text::TRNAME[1] => @user.trainer_name) unless mega_evolution_with_mega_stone
+
         return parse_text_with_pokemon(
           19, 1165, @user,
           PFM::Text::PKNICK[0] => @user.given_name,
           PFM::Text::ITEM2[2] => @user.item_name,
           PFM::Text::TRNAME[1] => @user.trainer_name,
           PFM::Text::ITEM2[3] => @scene.logic.mega_evolve.mega_tool_name(@user)
+        )
+      end
+
+      # Get the post mega evolve message
+      # @return [String]
+      def post_mega_evolution_message
+        return parse_text_with_pokemon(
+          19, 1168, @user,
+          PFM::Text::PKNAME[1] => @user.given_name
         )
       end
     end
