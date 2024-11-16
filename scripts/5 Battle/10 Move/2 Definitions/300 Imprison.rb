f1c6477cec322b49b6f58ca547037b9041b05f8a
Zøzo <benbar2412@gmail.com>
2024-11-16 14:42:39 +0000

    Fix Imprison (GEN 5 onwards)

--- scripts/5 Battle/10 Move/2 Definitions/300 Imprison.rb
+++ scripts/5 Battle/10 Move/2 Definitions/300 Imprison.rb
@@ -1,6 +1,6 @@
 module Battle
   class Move
-    # Opponent is unable to use moves that the user also knows.
+    # Class managing Imprison move
     class Imprison < Move
       # Function that tests if the user is able to use the move
       # @param user [PFM::PokemonBattler] user of the move
@@ -10,7 +10,6 @@ module Battle
       def move_usable_by_user(user, targets)
         return false unless super
         return show_usage_failure(user) && false if user.effects.has?(:imprison)
-        return show_usage_failure(user) && false unless has_common_moves?(user, targets)
 
         return true
       end
@@ -24,8 +23,8 @@ module Battle
         actual_targets.each do |target|
           next if target.effects.has?(:imprison)
 
-          user.effects.add(Effects::Imprison.new(logic, user))
-          scene.display_message_and_wait(deal_message(user, target))
+          user.effects.add(Effects::Imprison.new(@logic, user))
+          @scene.display_message_and_wait(deal_message(user, target))
         end
       end
 
@@ -35,19 +34,6 @@ module Battle
       # @return [String]
       def deal_message(user, actual_targets)
         return parse_text_with_pokemon(19, 586, user)
-      end
-
-      # Checks if the user and any target have common moves
-      # @param user [PFM::PokemonBattler] user of the move
-      # @param targets [Array<PFM::PokemonBattler>] potential targets
-      # @return [Boolean] true if there are common moves, false otherwise
-      def has_common_moves?(user, targets)
-        user_move_db_symbols = user.moveset.map(&:db_symbol)
-
-        return targets.any? do |target|
-          target_move_db_symbols = target.moveset.map(&:db_symbol)
-          (user_move_db_symbols.intersection(target_move_db_symbols)).any?
-        end
       end
     end
 
